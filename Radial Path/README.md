40c14a99515892bb7fd9ca06d79cdac91d909d3d
borntofrappe <borntofrappe@gmail.com>
2020-03-24 09:49:10 +0100

    structure visualization and add markup

--- Radial Path/README.md
+++ Radial Path/README.md
@@ -6,24 +6,49 @@ With this project I set out to practice with several features of the D3 library,
 
 - **Experiment** takes the static foundation and experiments with a constantly updated set of data.
 
-## Data Viz
+## Goal
 
 The idea is to plot the number of deaths registered in Italy due to the recent Coronavirus spread. This using data from [the Covid19 API](https://covid19api.com/), but starting from a fixed set of data. A fetch request can follow to update the visualization if necessary.
 
 A radial line chart is here included next to a normal line chart to describe two facets of the dataset, namely its cumulative, and marginal distribution.
 
-## Shape Functions
+## Markup
 
-The line and area functions work similarly to the non-radial counterpart. You just need to adjust your thinking from `x` and `y` to:
+> A bit of planning a long way goes
+
+```pug
+div root
+  h1 title
+  p copy
+  div wrapper
+    p brief
+    svg line chart
+  div wrapper
+    p brief
+    svg radial line chart
+```
+
+## D3
+
+The script makes use of several functions from D3, and most notably `d3.line`, `d3.lineRadial`. There are however other useful methods picked up from the library:
+
+- `format` and `timeFormat`, to format the values and dates scattered throughout the project
+
+- `scaleLinear`, `scaleTime`, to position the elements in the area described by the SVG element
+
+### d3-shape
+
+Feature of this particular project, the radial line chart functions similarly to the non-radial counterpart. Instead of using `x` and `y` values however, the function depends on two different arguments:
 
 - `angle`: the rotation around the `[0, Math.PI * 2]` circle
 
 - `radius`: the distance from the center
 
-The center is assumed to be where the path is within the `viewBox` of the SVG, so be sure to translate the shape accordingly.
+Be aware that the point in the SVG element where you append the path element is considered the center of the visualization. In light of this, use group elements or the `viewBox` attribute to translate the shape where needed.
 
-While the radius can use a linear scale then, the angle takes advantage of an ordinal scale, so to map each successive data point to a value on the defined `Math.PI * 2` spectrum.
+## Docs
 
-## Helpful links
-
-- [lineRadial Docs](https://github.com/d3/d3-shape#lineRadial)
+- [d3-format](https://github.com/d3/d3-format)
+- [d3-timeFormat](https://github.com/d3/d3-time-format)
+- [d3-scale](https://github.com/d3/d3-scale)
+- [d3-shape](https://github.com/d3/d3-shape), most prominently [d3.lineRadial](https://github.com/d3/d3-shape#lineRadial) and [d3.areaRadial](https://github.com/d3/d3-shape#areaRadial)
--- Radial Path/script.js
+++ Radial Path/script.js
@@ -1,12 +1,32 @@
+const { select, format } = d3;
+
 // fixed set of data
 // the idea is to consider fetching additional information if the latest observation is more than a day old
 const data = [{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-21T00:00:00Z","Cases":1,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-22T00:00:00Z","Cases":2,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-23T00:00:00Z","Cases":3,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-24T00:00:00Z","Cases":7,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-25T00:00:00Z","Cases":10,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-26T00:00:00Z","Cases":12,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-27T00:00:00Z","Cases":17,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-28T00:00:00Z","Cases":21,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-02-29T00:00:00Z","Cases":29,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-01T00:00:00Z","Cases":34,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-02T00:00:00Z","Cases":52,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-03T00:00:00Z","Cases":79,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-04T00:00:00Z","Cases":107,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-05T00:00:00Z","Cases":148,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-06T00:00:00Z","Cases":197,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-07T00:00:00Z","Cases":233,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-08T00:00:00Z","Cases":366,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-09T00:00:00Z","Cases":463,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-10T00:00:00Z","Cases":631,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-11T00:00:00Z","Cases":827,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-12T00:00:00Z","Cases":827,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-13T00:00:00Z","Cases":1266,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-14T00:00:00Z","Cases":1441,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-15T00:00:00Z","Cases":1809,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-16T00:00:00Z","Cases":2158,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-17T00:00:00Z","Cases":2503,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-18T00:00:00Z","Cases":2978,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-19T00:00:00Z","Cases":3405,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-20T00:00:00Z","Cases":4032,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-21T00:00:00Z","Cases":4825,"Status":"deaths"},{"Country":"Italy","Province":"","Lat":43,"Lon":12,"Date":"2020-03-22T00:00:00Z","Cases":5476,"Status":"deaths"}];
 const url = "https://api.covid19api.com/dayone/country/italy/status/deaths";
 
+const dayOne = new Date(data[0].Date);
+const dayLast = new Date(data[data.length - 1].Date);
+const formatPercentage = format("%");
+
+const root = select('#root')
+root.append('h1').text('Italy - Covid19');
+root.append('p').text(`Highlighting the number of deaths in the peninsular region between ${dayOne.toDateString()} and ${dayLast.toDateString()}`);
+
+
 /* line chart
 plot cumulative distribution
-
 */
+const wrapperLineChart = root.append('div');
+wrapperLineChart.append('h2').text('In total')
+// milliseconds -> days
+wrapperLineChart.append('p').text(`The country has registered ${data[data.length - 1].Cases} cases in the span of ${(dayLast - dayOne) / 1000 / 60 / 60 / 24} days`);
+
+// VIZ #1
+const width = 500;
+const height = 400;
+const yScale = scaleLinear().domain([0, max(data, d => d.Cases)]).range([0, width]);
+const xScale = scaleTime().domain(extent(data, d => new Date(d.Date))).range([height, 0]);
 
 
 /* radial line chart
@@ -22,10 +42,16 @@ const dataRadial = data.reduce((acc, curr) => {
     const { cases } = acc[acc.length - 1];
     acc.push({
       cases: curr.Cases,
-      growth: curr.Cases / cases,
+      growth: curr.Cases / cases - 1,
     });
   }
   return acc;
 } , []);
 
-console.log(dataRadial);
+
+const wrapperRadialChart = root.append('div');
+wrapperRadialChart.append('h2').text('Day over day')
+// milliseconds -> days
+wrapperRadialChart.append('p').text(`The number of cases increased ${formatPercentage(dataRadial[dataRadial.length - 1].growth)} in the last day of observation`);
+
+// VIZ #2
