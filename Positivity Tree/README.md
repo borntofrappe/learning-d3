cc1190a2c249eabf08a27269445db6481db7f547
borntofrappe <borntofrappe@gmail.com>
2020-04-12 23:59:22 +0200

    complete visualization

--- Positivity Tree/README.md
+++ Positivity Tree/README.md
@@ -1,13 +1,78 @@
 # Positivity Tree
 
-## Notes
-
 This project is inspired by one of the visuals shown [in this article](https://www.lemonde.fr/les-decodeurs/article/2020/04/07/les-enjeux-du-deconfinement-expliques-en-schemas_6035827_4355770.html) on [lemonde.fr](https://www.lemonde.fr/), and more specifically the graphic showing how a virus is able to spread to more and more people with each passing generation.
 
 I thought it would be a great way to practice with the `d3-tree` module, using the concept with a more uplifting visualization. The idea is to start with the visual described in `face-smiling.svg`, and have the smile spread to more data points. `face-smiling.svg` as opposed to the visual behind `face-default.svg`.
 
-## Responsiveness
+## Feature Request
 
-On top of the tree diagram, it seems the developers behind the article have made it possible to change the direction of the visualization. On smaller screens, the data is read top to bottom instead of left to right.
+Once you recreate the tree structure, try to change the orientation of the visualization for mobile devices (below an arbitrary threshold). It seems the developers behind the article have made it possible to map the data top to bottom instead of left to right, just for this scenario.
 
-Try to recreate the feature.
+## Notes
+
+> To my future self, a few things worth your time
+
+### Faces
+
+To draw the faces I decided to experiment with the `<defs>` ,`<symbol>` and `<use>` elements.
+
+1. define a symbol
+
+   ```html
+   <svg>
+     <defs>
+       <symbol id="face-smiling" viewBox="-50 -50 100 100"></symbol>
+     </defs>
+   </svg>
+   ```
+
+   By default, nothing within the `defs` block will be drawn on the screen. Notice that the `symbol` element has a `viewBox`, which is applied to the nested drawing elements, and an `id` attribute, which will come in handy in a moment.
+
+2. describe the shape with circle, path, rect... and other drawing elements
+
+   ```html
+   <svg>
+     <defs>
+       <symbol viewBox="-50 -50 100 100">
+         <circle r="47" stroke="currentColor" stroke-width="6" fill="none" />
+         <circle r="5" cx="-15" cy="-6" />
+         <circle r="5" cx="15" cy="-6" />
+         <path d="M -10 15 h 20" stroke="currentColor" stroke-width="4" fill="none" />
+       </symbol>
+     </defs>
+   </svg>
+   ```
+
+3. use the symbol
+
+   ```html
+   <svg>
+     <defs>
+       <symbol viewBox="-50 -50 100 100">
+       <!-- drawing elements -->
+       </symbol>
+     </defs>
+
+     <use href="#face-smiling">
+   </svg>
+   ```
+
+   Notice how `href` refers to the value previously set in the `id` attribute.
+
+4. resize as needed
+
+   ```html
+   <svg>
+     <use href="#face-smiling" width="80" height="80">
+   </svg>
+   ```
+
+   The `width` and `height` attribute are relative to the wrapping `<svg>` element and its `viewBox`. **Pay attention** on where the symbol is drawn however. Even though the `symbol` has a particular `viewBox`, the shape will be drawn starting from the top left corner. To center the shape in the coordinate system, offset by half the width and half the height
+
+   ```html
+   <svg>
+     <use href="#face-smiling" x="-40" y="-40" width="80" height="80">
+   </svg>
+   ```
+
+   Use the `transform` attribute alternatively to `x` and `y`
--- Positivity Tree/script.js
+++ Positivity Tree/script.js
@@ -1,1 +1,90 @@
-// good luck
+// DATA
+/* there is definitely a better way to do this, but the idea is to
+
+1. start from an object
+{
+  isSmiling: true,
+  children: []
+}
+
+1. add a generation of five children, of which three are smiling
+
+1. add another generation for the smiling children
+1. repeat once more
+*/
+function getGeneration(children = 5, numberPositive = 3) {
+  let generation = [];
+  for (let i = 0; i < children; i += 1) {
+    const point = {
+      isSmiling: !(i < children - numberPositive),
+    };
+
+    const index = Math.round(Math.random() * generation.length);
+    generation = [
+      ...generation.slice(0, index),
+      point,
+      ...generation.slice(index),
+    ];
+  }
+  return generation;
+}
+
+function getData() {
+  const data = {
+    isSmiling: true,
+    children: [],
+  };
+
+  const generation = getGeneration();
+  data.children = generation;
+
+  const nextGenerations = generation.filter(({ isSmiling }) => isSmiling);
+  nextGenerations.forEach(nextGeneration => {
+    nextGeneration.children = getGeneration();
+    const nextGenerations = generation.filter(({ isSmiling }) => isSmiling);
+    nextGenerations.forEach(nextGeneration => {
+      nextGeneration.children = getGeneration();
+    });
+  });
+
+  return data;
+}
+
+const data = getData();
+
+
+// VIZ
+const main = d3.select('main');
+main.append('h1').text('Positivity Tree');
+
+const margin = 50;
+const width = 800;
+const height = 500;
+const svg = main.append('svg').attr('viewBox', `${-margin} ${-margin} ${width + margin * 2} ${height + margin * 2}`);
+
+// define the shapes to later <use> as needed
+const defs = svg.append('defs');
+const faceSmiling = defs.append('symbol').attr('viewBox', '-50 -50 100 100').attr('id', 'face-smiling');
+faceSmiling.append('circle').attr('r', '47').attr('stroke', 'currentColor').attr('stroke-width', '6').attr('fill', 'hsl(45, 90%, 85%)');
+faceSmiling.append('circle').attr('r', '5').attr('cx', '-15').attr('cy', '-6')
+faceSmiling.append('circle').attr('r', '5').attr('cx', '15').attr('cy', '-6')
+faceSmiling.append('path').attr('d', 'M -10 10 a 10 10 0 0 0 20 0').attr('stroke', 'currentColor').attr('stroke-width', '4').attr('fill', 'none');
+
+const faceDefault = defs.append('symbol').attr('viewBox', '-50 -50 100 100').attr('id', 'face-default');
+faceDefault.append('circle').attr('r', '47').attr('stroke', 'currentColor').attr('stroke-width', '6').attr('fill', 'hsl(45, 90%, 98%)');
+faceDefault.append('circle').attr('r', '5').attr('cx', '-15').attr('cy', '-6')
+faceDefault.append('circle').attr('r', '5').attr('cx', '15').attr('cy', '-6')
+faceDefault.append('path').attr('d', 'M -10 15 h 20').attr('stroke', 'currentColor').attr('stroke-width', '4').attr('fill', 'none');
+
+const hierarchy = d3.hierarchy(data);
+const tree = d3.tree().size([width, height]);
+const dataTree = tree(hierarchy);
+
+const links = dataTree.links();
+const descendants = dataTree.descendants();
+
+const link = d3.linkVertical().x(d => d.x).y(d => d.y);
+svg.selectAll('path.highlight').data(links.filter(link => link.target.data.isSmiling)).enter().append('path').attr('class', 'highlight').attr('d', d => link(d)).attr('fill', 'none').attr('stroke', 'hsl(45, 90%, 85%)').attr('stroke-width', '25');
+svg.selectAll('path.connection').data(links).enter().append('path').attr('class', 'connection').attr('d', d => link(d)).attr('fill', 'none').attr('stroke', 'currentColor').attr('stroke-width', '2');
+const size = 50;
+svg.selectAll('use').data(descendants).enter().append('use').attr('x', d => d.x).attr('y', d => d.y).attr('width', size).attr('height', size).attr('transform', `translate(${size / 2 * -1} ${size / 2 * -1})`).attr('href', d => d.data.isSmiling ? '#face-smiling' : '#face-default');
