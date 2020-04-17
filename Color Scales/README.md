ee712c7af12759dc9a9fca14bb43365c3c717be4
borntofrappe <borntofrappe@gmail.com>
2020-04-17 23:59:31 +0200

    complete the first viz showing a continuous color scale

--- Color Scales/README.md
+++ Color Scales/README.md
@@ -3,3 +3,29 @@
 ## Notes
 
 `d3-scale-chromatic` offers several options to create a color palette. With this demo I set out to explore the module, and highlight the different types of scales you can set up with it.
+
+## Github Contributions
+
+Here I use a _continuous_ scale.
+
+```js
+const colorScale = d3.scaleSequential(d3.interpolateGreens);
+```
+
+The function provides a darker and darker shade of green as you approach the end of the `[0, 1]` range.
+
+```js
+colorScale(0); // rgb(247, 252, 245)
+colorScale(1); // rgb(0, 68, 27)
+```
+
+This makes the scale a prime candidate to map data returned from the `Math.random()` function.
+
+### Domain
+
+The default domain is `[0, 1]`, but as with any other scale, you can modify this value with the `.domain` function.
+
+```js
+const colorScale = d3.scaleSequential(d3.interpolateGreens).domain([0, 5]);
+colorScale(1); // rgb(211, 238, 205)
+```
--- Color Scales/script.js
+++ Color Scales/script.js
@@ -1,1 +1,38 @@
-// good luck
+const main = d3.select('main');
+main.append('h1').text('Github Contributions');
+main.append('p')
+  .text('A perfect use case for a continuous color scale: ')
+  .append('a')
+  .attr('href', 'https://github.com/d3/d3-scale-chromatic#interpolateGreens')
+  .append('code')
+  .text('d3.interpolateGreens.');
+
+const dataPoints = 100;
+const rows = 7;
+const columns = Math.ceil(dataPoints / rows);
+const data = Array(dataPoints).fill().map((d, i) => ({
+  value: Math.random(),
+  column: Math.floor(i / rows),
+  row: i % rows,
+}));
+
+const width = 400;
+const height = width / columns * rows;
+
+// map values in the [0, 1] interval
+const colorScale = d3.scaleSequential(d3.interpolateGreens);
+
+const xScale = d3.scaleBand().domain(d3.range(columns)).range([0, width]).padding(0.1);
+const yScale = d3.scaleBand().domain(d3.range(rows)).range([0, height]).padding(0.1);
+
+const svg = main.append('svg').attr('viewBox', `0 0 ${width} ${height}`).attr('width', width).attr('height', height);
+svg
+  .selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', d => xScale(d.column))
+  .attr('y', d => yScale(d.row))
+  .attr('width', xScale.bandwidth())
+  .attr('height', yScale.bandwidth())
+  .attr('fill', d => colorScale(d.value));
