c6713cbbf8dc5f99e91cb98f1ab1aef8bf429f5f
borntofrappe <borntofrappe@gmail.com>
2020-04-18 23:22:31 +0200

    complete visualization

--- Color Scales/README.md
+++ Color Scales/README.md
@@ -4,28 +4,47 @@
 
 `d3-scale-chromatic` offers several options to create a color palette. With this demo I set out to explore the module, and highlight the different types of scales you can set up with it.
 
-## Github Contributions
+## Scattered symbols
 
-Here I use a _continuous_ scale.
+Three different data sets are highlighted in a scatter plot. I use the `d3-shape` module to map the data points with a variety of symbols, and a discrete color scale to differentiate the visuals in color.
 
 ```js
-const colorScale = d3.scaleSequential(d3.interpolateGreens);
+const colorScale = d3.schemeCategory10;
 ```
 
-The function provides a darker and darker shade of green as you approach the end of the `[0, 1]` range.
+`schemeCategory10` provides an array of colors. You can then use the array in an ordinal scale to map a set of categories to the individual picks.
 
 ```js
-colorScale(0); // rgb(247, 252, 245)
-colorScale(1); // rgb(0, 68, 27)
+const symbols = ["circle", "square", "wye", "cross"];
+const colorScaleScatter = d3.scaleOrdinal(d3.schemeCategory10).domain(symbols);
 ```
 
-This makes the scale a prime candidate to map data returned from the `Math.random()` function.
-
-### Domain
-
-The default domain is `[0, 1]`, but as with any other scale, you can modify this value with the `.domain` function.
+For the symbol, the API uses two attributes: `size` and `type`. The first one is set globally on the symbol generator.
 
 ```js
-const colorScale = d3.scaleSequential(d3.interpolateGreens).domain([0, 5]);
-colorScale(1); // rgb(211, 238, 205)
+const symbol = d3.symbol().size(82);
+```
+
+The second one is updated according to the input type. Annoyingly enough, the library uses types as `d3.symbolCircle` and `d3.symbolWye`, with camelCase notation.
+
+## Github contributions
+
+Here I map an arbitrary number of data points to a series of squares. This to replicate the layout of Github contributions. In terms of colors, I use a _continuous_ scale for a range of green colors.
+
+```js
+const colorScaleGithub = d3.scaleSequential(d3.interpolateGreens);
+```
+
+The function provides a darker and darker shade of green as you approach the end of the `[0, 1]` interval.
+
+```js
+colorScaleGithub(0); // rgb(247, 252, 245)
+colorScaleGithub(1); // rgb(0, 68, 27)
+```
+
+This makes the scale an excellent way to map the values returned from the `Math.random()` function. The default domain is `[0, 1]`, but as with any other scale, you can modify this value with the `.domain` function.
+
+```js
+const colorScaleGithub = d3.scaleSequential(d3.interpolateGreens).domain([0, 5]);
+colorScaleGithub(1); // rgb(211, 238, 205)
 ```
--- Color Scales/script.js
+++ Color Scales/script.js
@@ -1,38 +1,86 @@
-const main = d3.select('main');
-main.append('h1').text('Github Contributions');
-main.append('p')
-  .text('A perfect use case for a continuous color scale: ')
-  .append('a')
-  .attr('href', 'https://github.com/d3/d3-scale-chromatic#interpolateGreens')
-  .append('code')
-  .text('d3.interpolateGreens.');
+const root = d3.select('#root');
 
-const dataPoints = 100;
+const header = root.append('header');
+header.append('h1').text('d3-scale-chromatic');
+header
+  .append('p')
+  .text('D3 offers a variety of color palette to style data visualizations');
+
+// discrete color scale - scatter plot
+const articleScatter = root.append('article')
+articleScatter.append('h2').text('Categorical scale');
+articleScatter.append('p').text('With an ordinal scale, different categories are mapped to different colors.');
+// plot three categories of data in two dimensions
+const symbol = d3.symbol().size(82);
+const randomData = (dataPoints) => Array(dataPoints).fill().map(() => ({
+  x: Math.random(),
+  y: Math.random(),
+}));
+const dataPointsScatter = 15;
+const symbols = ['circle', 'square', 'wye', 'cross'];
+const dataScatter = symbols.map(type => ({type, data: randomData(dataPointsScatter)}));
+const margin = 20;
+const widthScatter = 300;
+const heightScatter = 300;
+
+const colorScaleScatter = d3.scaleOrdinal(d3.schemeCategory10).domain(symbols);
+
+// continuous domain, continuous range
+const xScaleScatter = d3.scaleLinear().range([0, widthScatter]).nice();
+const yScaleScatter = d3.scaleLinear().range([heightScatter, 0]).nice();
+
+const svgScatter = articleScatter.append('svg').attr('viewBox', `${-margin} ${-margin} ${widthScatter + margin * 2} ${heightScatter + margin * 2}`).attr('width', widthScatter).attr('height', heightScatter);
+const groupsScatter = svgScatter.selectAll('g').data(dataScatter).enter().append('g').attr('data-type', d => d.type);
+
+groupsScatter
+  .selectAll('path')
+  .data(d => d.data)
+  .enter()
+  .append('path')
+  .attr('transform', ({x, y}) => `translate(${xScaleScatter(x)} ${yScaleScatter((y))})`)
+  .attr('fill', function() {
+    const type = this.parentNode.getAttribute('data-type');
+    return colorScaleScatter(type);
+  })
+  .attr('d', function() {
+    const type = this.parentNode.getAttribute('data-type');
+    symbol.type(d3[`symbol${type[0].toUpperCase()}${type.slice(1)}`]);
+    return symbol();
+  });
+
+// continuous color scale - github contributions
+const articleGithub = root.append('article');
+articleGithub.append('h2').text('Continuous scale');
+articleGithub.append('p').text('With a sequential scale, a set of values is mapped to different shades of the same color');
+// plot an arbitrary number of data points in a grid of squares
+const dataPointsGithub = 100;
 const rows = 7;
-const columns = Math.ceil(dataPoints / rows);
-const data = Array(dataPoints).fill().map((d, i) => ({
+const columns = Math.ceil(dataPointsGithub / rows);
+const dataGitub = Array(dataPointsGithub).fill().map((d, i) => ({
   value: Math.random(),
   column: Math.floor(i / rows),
   row: i % rows,
 }));
 
-const width = 400;
-const height = width / columns * rows;
+const widthGithub = 400;
+const heightGithub = widthGithub / columns * rows;
 
-// map values in the [0, 1] interval
-const colorScale = d3.scaleSequential(d3.interpolateGreens);
+// conveniently, the default domain is [0, 1]
+const colorScaleGithub = d3.scaleSequential(d3.interpolateGreens);
 
-const xScale = d3.scaleBand().domain(d3.range(columns)).range([0, width]).padding(0.1);
-const yScale = d3.scaleBand().domain(d3.range(rows)).range([0, height]).padding(0.1);
+// discrete domain, continuous range
+const xScaleGithub = d3.scaleBand().domain(d3.range(columns)).range([0, widthGithub]).padding(0.2);
+const yScaleGithub = d3.scaleBand().domain(d3.range(rows)).range([0, heightGithub]).padding(0.2);
 
-const svg = main.append('svg').attr('viewBox', `0 0 ${width} ${height}`).attr('width', width).attr('height', height);
-svg
+const svgGithub = articleGithub.append('svg').attr('viewBox', `0 0 ${widthGithub} ${heightGithub}`).attr('width', widthGithub).attr('height', heightGithub);
+svgGithub
   .selectAll('rect')
-  .data(data)
+  .data(dataGitub)
   .enter()
   .append('rect')
-  .attr('x', d => xScale(d.column))
-  .attr('y', d => yScale(d.row))
-  .attr('width', xScale.bandwidth())
-  .attr('height', yScale.bandwidth())
-  .attr('fill', d => colorScale(d.value));
+  .attr('x', d => xScaleGithub(d.column))
+  .attr('rx', 1)
+  .attr('y', d => yScaleGithub(d.row))
+  .attr('width', xScaleGithub.bandwidth())
+  .attr('height', yScaleGithub.bandwidth())
+  .attr('fill', d => colorScaleGithub(d.value));
