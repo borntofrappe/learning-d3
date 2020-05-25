a1fd5b1b625f42320be21d31f586629b0f4826ee
borntofrappe <borntofrappe@gmail.com>
2020-05-25 16:13:36 +0200

    introduce spider chart project

---
+++ Spider Chart/script.js
@@ -0,0 +1,5 @@
+const main = d3.select('main');
+main.append('h1').text('Spider Chart');
+
+// map arbitrary values around a set of days
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
