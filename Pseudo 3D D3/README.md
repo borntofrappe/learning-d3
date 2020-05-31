622e2c7320dccd1b23c8b206098ed2f3a106ef82
borntofrappe <borntofrappe@gmail.com>
2020-05-31 23:42:32 +0200

    complete visualization

--- Pseudo 3D D3/README.md
+++ Pseudo 3D D3/README.md
@@ -8,4 +8,8 @@ Following the experimentation introduced in the **Faux 3D D3** folder, I take th
 
 ### Data
 
-For the data I picked the production of ice cream in Europe. eurostat has information for the 2018 year, highlighting the biggest producers [right here](https://ec.europa.eu/eurostat/en/web/products-eurostat-news/-/DDN-20190826-1). For the numbers, refer to [this table](https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-066341_QID_2364182F_UID_-3F171EB0&layout=INDICATORS,C,X,0;DECL,L,Y,0;PRCCODE,B,Z,0;PERIOD,L,Z,1;&zSelection=DS-066341PERIOD,201852;DS-066341PRCCODE,10521000;&rankName1=PRCCODE_1_0_-1_2&rankName2=PERIOD_1_0_0_0&rankName3=INDICATORS_1_2_0_0&rankName4=DECL_1_0_0_1&sortR=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23).
+For the data I picked the production of ice cream in Europe. eurostat has information for the 2018 year, highlighting the largest producers [right here](https://ec.europa.eu/eurostat/en/web/products-eurostat-news/-/DDN-20190826-1). For the numbers, refer to [this table](https://appsso.eurostat.ec.europa.eu/nui/show.do?query=BOOKMARK_DS-066341_QID_2364182F_UID_-3F171EB0&layout=INDICATORS,C,X,0;DECL,L,Y,0;PRCCODE,B,Z,0;PERIOD,L,Z,1;&zSelection=DS-066341PERIOD,201852;DS-066341PRCCODE,10521000;&rankName1=PRCCODE_1_0_-1_2&rankName2=PERIOD_1_0_0_0&rankName3=INDICATORS_1_2_0_0&rankName4=DECL_1_0_0_1&sortR=ASC_-1_FIRST&rStp=&cStp=&rDCh=&cDCh=&rDM=true&cDM=true&footnes=false&empty=false&wai=false&time_mode=ROLLING&time_most_recent=false&lang=EN&cfo=%23%23%23%2C%23%23%23.%23%23%23).
+
+### Shapes
+
+In a first version, draw a series of cones, side by side. Change the scale of the cone according to the input data.
--- Pseudo 3D D3/script.js
+++ Pseudo 3D D3/script.js
@@ -1,2 +1,102 @@
+const data = [
+  {
+    country: 'France',
+    value: 450600078,
+    colors: ['hsl(220, 85%, 65%)', 'hsl(0, 0%, 98%)', 'hsl(0, 90%, 60%)'],
+  },
+  {
+    country: 'Germany',
+    value: 493945671,
+    colors: ['hsl(0, 0%, 30%)', 'hsl(0, 90%, 60%)', 'hsl(60, 95%, 65%)'],
+  },
+  {
+    country: 'Italy',
+    value: 435463054,
+    colors: ['hsl(120, 80%, 70%)', 'hsl(0, 0%, 98%)', 'hsl(0, 90%, 60%)'],
+  },
+  {
+    country: 'United Kingdom',
+    value: 328055041,
+    colors: ['hsl(0, 0%, 98%)', 'hsl(0, 90%, 60%)', 'hsl(220, 85%, 65%)'],
+  },
+  {
+    country: 'Spain',
+    value: 307065000,
+    colors: ['hsl(0, 90%, 60%)', 'hsl(60, 95%, 65%)', 'hsl(0, 90%, 60%)'],
+  },
+].sort((a, b) => a.value > b.value ? -1 : 1);
+
+const canvas = document.createElement('canvas');
+const width = 800;
+const height = 250;
+canvas.width = width;
+canvas.height = height;
+
+document.querySelector('main').appendChild(canvas);
+
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 20,
+  left: 20
+};
+const scaleSize = d3.scaleLinear().domain([0, d3.max(data, d => d.value)]).range([1, 5]);
+const scalePosition = d3.scaleBand().domain(data.map(({country}) => country)).range([width - margin.right, margin.left])
+
+const illustration = new Zdog.Illustration({
+  element: canvas,
+  translate: { y: -20 },
+  rotate: { y : -Zdog.TAU / 20, x: -Zdog.TAU / 50 },
+});
+
+data.forEach(({country, value, colors}) => {
+  const size = scaleSize(value);
+  const position = scalePosition(country) + scalePosition.bandwidth() / 2;
+  const length = 20;
+  const anchor = new Zdog.Anchor({
+    addTo: illustration,
+    translate: { x: position - width / 2, y: length * scaleSize.range()[1] },
+    scale: size,
+  });
+
+  new Zdog.Cone({
+    addTo: anchor,
+    diameter: 15,
+    length,
+    stroke: 15,
+    color: 'hsl(38, 85%, 55%)',
+    translate: { y: -length },
+    // translate: { y: size * 40 },
+    rotate: { x: -Zdog.TAU / 4},
+  });
+
+  const scoop = new Zdog.Group({
+    addTo: anchor,
+    translate: { y: -length },
+  })
+  
+  colors.forEach((color, i) => {
+    new Zdog.Shape({
+      addTo: scoop,
+      stroke: 60,
+      color: color,
+      translate: { x: -6 + 6 * i, y: i % 2 === 0 ? -2 : -6 },
+    })
+  });
+
+});
+
+
+illustration.updateRenderGraph();
+
+const format = d3.format(",");
+
 const main = d3.select('main');
-main.append('h1').text('Bonne chance');
+
+main.append('p').text(`In Europe, ${data[0].country} is the largest producer of ice cream. In terms of quantity, the top five countries can be summarised as follows:`);
+const table = main.append('table');
+
+table.append('thead').append('tr').selectAll('th').data(['Country', 'Quantity (million liters)']).enter().append('th').text(d => d);
+table.append('tbody').selectAll('tr').data(data).enter().append('tr');
+d3.selectAll('tbody tr').append('td').text(d => d.country);
+d3.selectAll('tbody tr').append('td').text(d => format(d.value)).style('text-align', 'end');
