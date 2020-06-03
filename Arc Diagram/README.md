5a506e11452ae7fa7743863da4d40e8320d9a5ec
borntofrappe <borntofrappe@gmail.com>
2020-06-03 23:08:26 +0200

    introduce arc project

---
+++ Arc Diagram/README.md
@@ -0,0 +1,15 @@
+# Arc Diagram
+
+<!-- ## [Live Demo]() -->
+
+Based on the network structure highlighted in [this project](https://codepen.io/borntofrappe/pen/qQZyor) create a diagram connecting the nodes side by side and with a series of arcs.
+
+## Notes
+
+Consider this a direct follow-up to the **Chord Diagram** project.
+
+## Links
+
+- [network diagram](https://codepen.io/borntofrappe/pen/qQZyor)
+
+- [chord diagram](https://codepen.io/borntofrappe/full/gOPObrL)
---
+++ Arc Diagram/script.js
@@ -0,0 +1,39 @@
+// manually picked from the settings tab @codepen
+const nodes = [
+    { name: 'HTML' },
+    { name: 'CSS' },
+    { name: 'JavaScript' },
+    { name: 'Haml' },
+    { name: 'Markdown' },
+    { name: 'Slim' },
+    { name: 'Pug' },
+    { name: 'LESS' },
+    { name: 'SCSS' },
+    { name: 'Sass' },
+    { name: 'Stylus' },
+    { name: 'PostCSS' },
+    { name: 'Babel' },
+    { name: 'TypeScript' },
+    { name: 'CoffeeScript' },
+    { name: 'LiveScript' },
+  ];
+  
+  // set html as the root node
+  const links = [
+    { source: 'HTML', target: 'CSS' },
+    { source: 'HTML', target: 'JavaScript' },
+    { source: 'HTML', target: 'Haml' },
+    { source: 'HTML', target: 'Markdown' },
+    { source: 'HTML', target: 'Slim' },
+    { source: 'HTML', target: 'Pug' },
+    { source: 'CSS', target: 'LESS' },
+    { source: 'CSS', target: 'SCSS' },
+    { source: 'CSS', target: 'Sass' },
+    { source: 'CSS', target: 'Stylus' },
+    { source: 'CSS', target: 'PostCSS' },
+    { source: 'JavaScript', target: 'Babel' },
+    { source: 'JavaScript', target: 'TypeScript' },
+    { source: 'JavaScript', target: 'CoffeeScript' },
+    { source: 'JavaScript', target: 'LiveScript' },
+  ];
+  
