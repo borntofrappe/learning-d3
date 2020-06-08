716d98f0092fe61313462356dcbf2d33e545fe75
borntofrappe <borntofrappe@gmail.com>
2020-06-08 18:31:24 +0200

    complete visualization

--- Histogram Bins/README.md
+++ Histogram Bins/README.md
@@ -1,5 +1,14 @@
 # Histogram Bins
 
 <!-- ## [Live Demo]() -->
+## Data
 
-Use the `.bin()` method to create a histogram.
+For the data, consider a few of the random functions provided by `d3-random`
+
+## bin
+
+It is necessary to import the specific module `d3-array` to have access to the `bin()` function.
+
+```html
+<script src="https://unpkg.com/d3-array@2.4.0/dist/d3-array.min.js"></script>
+```
--- Histogram Bins/script.js
+++ Histogram Bins/script.js
@@ -1,2 +1,81 @@
 const main = d3.select('main');
-main.append('h1').text('Bonne chance');
+const header = main.append('header');
+header.append('h1').text('Bins');
+header.append('p').html('Use <code>d3.bin</code> to slot a random set of data in a series of evenly-spaced containers.');
+
+// input values
+const datapoints = 300;
+const thresholds = 25;
+const mu = 0;
+const sigma = 0.5;
+
+
+const random = () => d3.randomLogNormal(mu, sigma)();
+const data = Array(datapoints).fill().map(() => random());
+const min = d3.min(data);
+const max = d3.max(data);
+
+const bin = d3.bin().thresholds(thresholds)
+const bins = bin(data);
+const articleHTML = main.append('article');
+articleHTML.append('h2').text('Input');
+
+const input = [
+    {
+        term: 'Function',
+        description: 'd3.randomLogNormal(mu, sigma)'
+    },
+    {
+        term: 'Data Points',
+        description: datapoints
+    },
+    {
+        term: 'Thresholds',
+        description: thresholds
+    },
+    {
+        term: 'mu',
+        description: mu
+    },
+    {
+        term: 'sigma',
+        description: sigma
+    },
+];
+
+const dl = articleHTML.append('dl');
+input.forEach(({term, description}) => {
+    dl.append('dt').text(term)
+    dl.append('dd').text(description)
+});
+
+const height = 400; 
+const width = 400;
+const xScale = d3.scaleBand().domain(d3.range(bins.length)).range([0, width]).padding(0.2)
+const yScale = d3.scaleLinear().domain([0, d3.max(bins, d => d.length)]).range([0, height])
+
+
+const articleSVG = main.append('article');
+articleSVG.append('h2').text('Output');
+
+const svg = articleSVG
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .attr('width', width)
+    .attr('height', height)
+
+    const groupBins = svg
+        .selectAll('g.bin')
+        .data(bins)
+        .enter()
+        .append('g')
+        .attr('class', 'bin')
+        .attr('transform', (d, i) => `translate(${xScale(i)} ${height})`);
+    
+    groupBins
+        .append('rect')
+        .attr('stroke', 'none')
+        .attr('fill', 'currentColor')
+        .attr('width', xScale.bandwidth())
+        .attr('height', (d) => yScale(d.length))
+        .attr('transform', 'scale(1 -1)');
