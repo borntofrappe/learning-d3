dce59d12a3903d87ba965e1e502c5a0c934f87ff
borntofrappe <borntofrappe@gmail.com>
2021-02-21 23:59:28 +0100

    code | football stats data

--- Football Stats/README.md
+++ Football Stats/README.md
@@ -5,3 +5,15 @@
 ## Notes
 
 Here I set out to create a couple of visualizations connected to football. The first analyzes the lackluster success of French teams in the European League competition, while the second wants to show the contribution of the players of the Paris-Saint-Germain team, and in the context of the Champions League competition. The first visualization is set to replicate the visual from [this article from lemonde.fr](https://www.lemonde.fr/les-decodeurs/article/2018/05/04/marseille-club-francais-recordman-des-finales-europeennes-depuis-1980_5294581_4355770.html), while the second is loosely inspired by [this other article from lemonde.fr](https://www.lemonde.fr/les-decodeurs/article/2021/02/16/en-un-graphique-le-parcours-du-psg-en-ligue-des-champions_6070145_4355770.html).
+
+## Squares
+
+The goal is to highlight the highest stage of competition reached by any French team in the European League. This with a series of squares layered on top of each other.
+
+```txt
+Final
+Semi-finals                 o
+Quarter-finals              o     o     o
+Second round                o     o  o  o
+Group stage or first round  o  o  o  o  o  o  o
+```
--- Football Stats/script.js
+++ Football Stats/script.js
@@ -1,2 +1,81 @@
 const main = d3.select('main');
 main.append('h1').text('Bonne chance');
+
+const states = ["Final", "Semi-finals", "Quarter-finals", "Eights-finals", "Group stage or previous rounds"]
+
+const data = {
+  '1980': { stage: 'Quarter-finals', teams: ['AS Saint-Etienne'] },
+  '1981': { stage: 'Semi-finals', teams: ['FC Sochaux'] },
+  '1982': { stage: 'Eights-finals', teams: ['Girondins de Bordeaux'] },
+  '1983': { stage: 'Eights-finals', teams: ['Girondins de Bordeaux'] },
+  '1984': { stage: 'Eights-finals', teams: ['RC Lens'] },
+  '1985': {
+    stage: 'Group stage or previous rounds',
+    teams: ['Paris Saint Germain'],
+  },
+  '1986': { stage: 'Quarter-finals', teams: ['FC Nantes'] },
+  '1987': { stage: 'Group stage or previous rounds', teams: ['FC Toulouse'] },
+  '1988': { stage: 'Group stage or previous rounds', teams: ['FC Toulouse'] },
+  '1989': { stage: 'Eights-finals', teams: ['Girondins de Bordeaux'] },
+  '1990': { stage: 'Quarter-finals', teams: ['AJ Auxerre'] },
+  '1991': {
+    stage: 'Eights-finals',
+    teams: ['AS Monaco', 'Girondins de Bordeaux'],
+  },
+  '1992': {
+    stage: 'Group stage or previous rounds',
+    teams: ['AJ Auxerre', 'AS Cannes', 'Olympique Lyonnais'],
+  },
+  '1993': {
+    stage: 'Semi-finals',
+    teams: ['AJ Auxerre', 'Paris Saint Germain'],
+  },
+  '1994': { stage: 'Eights-finals', teams: ['Girondins de Bordeaux'] },
+  '1995': { stage: 'Quarter-finals', teams: ['FC Nantes'] },
+  '1996': { stage: 'Final', teams: ['Girondins de Bordeaux'] },
+  '1997': { stage: 'Semi-finals', teams: ['AS Monaco'] },
+  '1998': { stage: 'Quarter-finals', teams: ['AJ Auxerre'] },
+  '1999': { stage: 'Final', teams: ['Olympique de Marseille'] },
+  '2000': { stage: 'Semi-finals', teams: ['RC Lens'] },
+  '2001': {
+    stage: 'Eights-finals',
+    teams: ['Girondins de Bordeaux', 'FC Nantes'],
+  },
+  '2002': { stage: 'Eights-finals', teams: ['Olympique Lyonnais', 'Lille OSC'] },
+  '2003': { stage: 'Eights-finals', teams: ['AJ Auxerre'] },
+  '2004': { stage: 'Final', teams: ['Olympique de Marseille'] },
+  '2005': { stage: 'Quarter-finals', teams: ['AJ Auxerre'] },
+  '2006': {
+    stage: 'Eights-finals',
+    teams: ['Lille OSC', 'AJ Auxerre', 'Olympique de Marseille'],
+  },
+  '2007': { stage: 'Eights-finals', teams: ['RC Lens', 'Paris Saint Germain'] },
+  '2008': { stage: 'Eights-finals', teams: ['Olympique de Marseille'] },
+  '2009': {
+    stage: 'Quarter-finals',
+    teams: ['Olympique de Marseille', 'Paris Saint Germain'],
+  },
+  '2010': {
+    stage: 'Eights-finals',
+    teams: ['Olympique de Marseille', 'Lille OSC'],
+  },
+  '2011': { stage: 'Eights-finals', teams: ['Paris Saint Germain'] },
+  '2012': {
+    stage: 'Group stage or previous rounds',
+    teams: ['Paris Saint Germain', 'Stade Rennais'],
+  },
+  '2013': { stage: 'Eights-finals', teams: ['Girondins de Bordeaux'] },
+  '2014': { stage: 'Quarter-finals', teams: ['Olympique Lyonnais'] },
+  '2015': { stage: 'Group stage or previous rounds', teams: ['EA Guingamp'] },
+  '2016': {
+    stage: 'Group stage or previous rounds',
+    teams: ['Olympique de Marseille', 'AS Saint-Etienne'],
+  },
+  '2017': { stage: 'Semi-finals', teams: ['Olympique Lyonnais'] },
+  '2018': { stage: 'Final', teams: ['Olympique de Marseille'] },
+  '2019': { stage: 'Group stage or previous rounds', teams: ['Stade Rennais'] },
+  '2020': {
+    stage: 'Group stage or previous rounds',
+    teams: ['Stade Rennais', 'AS Saint-Etienne'],
+  },
+};
