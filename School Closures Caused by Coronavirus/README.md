0bbc77c2debc9a8f86c72ffeb826fcd2ace761c8
borntofrappe <borntofrappe@gmail.com>
2021-04-19 23:57:08 +0200

    code - draw world with topojson

--- School Closures Caused by Coronavirus/README.md
+++ School Closures Caused by Coronavirus/README.md
@@ -5,3 +5,11 @@
 ## Notes
 
 [UNESCO](https://en.unesco.org) studies the impact of Covid-19 on education at a global scale with [an informative map](https://en.unesco.org/covid19/educationresponse). The goal of this project is to immediately replicate the geographical representation in order to practice with `d3-geo` and `topojson`. Following the static version, it might be interesting to develop a more elaborate demo, one contemplating a dashboard and or an animation to highlight different days.
+
+## Useful Links
+
+- [d3-geo](https://github.com/d3/d3-geo)
+
+- [topojson](https://github.com/topojson/topojson)
+
+- [topojson/world-atlas](https://github.com/topojson/world-atlas)
--- School Closures Caused by Coronavirus/script.js
+++ School Closures Caused by Coronavirus/script.js
@@ -1,2 +1,53 @@
-const main = d3.select('main');
-main.append('h1').text('Bonne chance');
+async function drawMap() {
+  const world = await d3.json(
+    'https://unpkg.com/world-atlas@2.0.2/countries-110m.json'
+  );
+
+  const width = 800;
+  const sphere = { type: 'Sphere' };
+  const projection = d3.geoNaturalEarth1().fitWidth(width, sphere);
+  const pathGenerator = d3.geoPath(projection);
+  const height = pathGenerator.bounds(sphere)[1][1];
+
+  const wrapper = d3
+    .select('main')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('viewBox', [0, 0, width, height]);
+
+  const bounds = wrapper.append('g');
+  // bounds
+  //   .append('path')
+  //   .attr('d', pathGenerator(sphere))
+  //   .attr('fill', 'hsl(220, 72%, 98%)')
+  //   .attr('stroke', 'none');
+
+  // const graticuleJson = d3.geoGraticule10();
+  // bounds
+  //   .append('path')
+  //   .attr('d', pathGenerator(graticuleJson))
+  //   .attr('fill', 'none')
+  //   .attr('stroke-width', '0.2')
+  //   .attr('stroke', 'hsl(220, 72%, 68%)');
+
+  const countries = topojson.feature(world, world.objects.countries);
+  bounds
+    .append('g')
+    .selectAll('path')
+    .data(countries.features.filter(d => d.properties.name !== 'Antarctica'))
+    .enter()
+    .append('path')
+    .attr('d', pathGenerator)
+    .attr('fill', 'hsl(206, 68%, 88%)')
+    .attr('stroke-width', '0.1')
+    .attr('stroke', 'hsl(214, 72%, 64%)')
+    .on('mouseenter', function() {
+      d3.select(this).attr('fill', 'hsl(40, 84%, 68%)');
+    })
+    .on('mouseleave', function() {
+      d3.select(this).attr('fill', 'hsl(206, 68%, 88%)');
+    });
+}
+
+drawMap();
