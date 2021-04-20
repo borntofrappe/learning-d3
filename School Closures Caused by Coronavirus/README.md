2581974fa89bbb968cdb43c5f701c41014eb1fae
borntofrappe <borntofrappe@gmail.com>
2021-04-20 22:55:49 +0200

    code - complete choropleth map

--- School Closures Caused by Coronavirus/README.md
+++ School Closures Caused by Coronavirus/README.md
@@ -1,10 +1,80 @@
 # School Closures Caused by Coronavirus
 
-<!-- ## [Live Demo]() -->
+[UNESCO](https://en.unesco.org) studies the impact of Covid-19 on education at a global scale with [a couple of informative maps](https://en.unesco.org/covid19/educationresponse). The goal of this project is to immediately replicate the second of these geographical representation, the one studying the duration of school closures, in order to practice with `d3-geo` and `topojson`.
 
-## Notes
+_Please note_: to read the data from `duration_school_closures.csv` it is necessary to run the project on a server.
 
-[UNESCO](https://en.unesco.org) studies the impact of Covid-19 on education at a global scale with [an informative map](https://en.unesco.org/covid19/educationresponse). The goal of this project is to immediately replicate the geographical representation in order to practice with `d3-geo` and `topojson`. Following the static version, it might be interesting to develop a more elaborate demo, one contemplating a dashboard and or an animation to highlight different days.
+```bash
+npm install live-server
+live-server
+```
+
+## World
+
+`topojson/world-data` provides the data necessary to draw the world and its countries.
+
+```js
+const world = await d3.json(
+  'https://unpkg.com/world-atlas@2.0.2/countries-110m.json'
+);
+```
+
+The data is in topojson format, and since the `d3-geo` works with geojson syntax, it is necessary to include the `topojson/topojson` module.
+
+```html
+<script src="https://unpkg.com/topojson@3.0.2/dist/topojson.js"></script>
+```
+
+An object with a type of `'Sphere'` is all that is required to draw the outline of the planet.
+
+```js
+const sphere = { type: 'Sphere' };
+```
+
+The projection describes how to map the data in the 2D plane, and it uses the sphere to understand the size of the visualization.
+
+```js
+const projection = d3.geoNaturalEarth1().fitWidth(width, sphere);
+```
+
+Based on the projection, `d3.geoPath` receives a geojson object to provide a fitting syntax for the `d` attribute of `<path>` elements.
+
+```js
+const pathGenerator = d3.geoPath(projection);
+```
+
+`pathGenerator.bounds` provides a two dimensional array with the dimensions of the input geometry. Through the sphere object, it allows to retrieve the height of the projection.
+
+```js
+const height = pathGenerator.bounds(sphere)[1][1];
+```
+
+With this information, it is finally possible to draw the map.
+
+- the object of type `'Sphere'` allows to draw the outline of the projection
+
+  ```js
+  pathGenerator(sphere);
+  ```
+
+- `d3.geoGraticule10()` provides an object to draw the langitude and longitude lines
+
+  ```js
+  pathGenerator(d3.geoGraticule10());
+  ```
+
+- the features from the retrieved topojson data provide the syntax for the countries
+
+  ```js
+  const countries = topojson.feature(world, world.objects.countries);
+  pathGenerator(countries[0]);
+  ```
+
+Ultimately I opted to just draw the countries (all, but Antarctica), but the instructions are worth remembering.
+
+## Data
+
+The data describing the number of weeks of full and partial school closure is retrieved from the [cited UNESCO source](https://en.unesco.org/covid19/educationresponse). It is important to note that the source is however modified to differentiate the countries with an `id` column. The change is necessary since the `ISO` value from the original dataset describes the country with a short string, and the value doesn't match the identifier provided by the topojson object. `id` replaces the label with with the [ISO 3166-1 numeric country code](https://en.wikipedia.org/wiki/ISO_3166-1_numeric).
 
 ## Useful Links
 
--- School Closures Caused by Coronavirus/script.js
+++ School Closures Caused by Coronavirus/script.js
@@ -3,28 +3,86 @@ async function drawMap() {
     'https://unpkg.com/world-atlas@2.0.2/countries-110m.json'
   );
 
+  const data = await d3.csv('./duration_school_closures.csv', function({
+    id,
+    country,
+    weeks,
+  }) {
+    return {
+      id,
+      country,
+      weeks: +weeks,
+    };
+  });
+
+  const dataMap = d3.group(data, d => d.id);
+
+  const groups = [
+    {
+      label: 'No data',
+      threshold: -Infinity,
+    },
+    {
+      label: '0 weeks',
+      threshold: 0,
+    },
+    {
+      label: '1-10 weeks',
+      threshold: 10,
+    },
+    {
+      label: '11-20 weeks',
+      threshold: 20,
+    },
+    {
+      label: '21-30 weeks',
+      threshold: 30,
+    },
+    {
+      label: '31-40 weeks',
+      threshold: 40,
+    },
+    {
+      label: '41+ weeks',
+      threshold: Infinity,
+    },
+  ];
+
+  const colors = d3.schemePuBu[groups.length];
+
   const width = 800;
   const sphere = { type: 'Sphere' };
   const projection = d3.geoNaturalEarth1().fitWidth(width, sphere);
   const pathGenerator = d3.geoPath(projection);
   const height = pathGenerator.bounds(sphere)[1][1];
 
-  const wrapper = d3
-    .select('main')
+  const root = d3.select('main');
+
+  const tooltip = root
+    .append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0);
+
+  root.append('h1').text('Total duration of school closures');
+
+  const wrapper = root
     .append('svg')
     .attr('width', width)
     .attr('height', height)
     .attr('viewBox', [0, 0, width, height]);
 
   const bounds = wrapper.append('g');
-  // bounds
+  const worldGroup = bounds.append('g');
+  const legendGroup = bounds.append('g');
+
+  // worldGroup
   //   .append('path')
   //   .attr('d', pathGenerator(sphere))
   //   .attr('fill', 'hsl(220, 72%, 98%)')
   //   .attr('stroke', 'none');
 
   // const graticuleJson = d3.geoGraticule10();
-  // bounds
+  // worldGroup
   //   .append('path')
   //   .attr('d', pathGenerator(graticuleJson))
   //   .attr('fill', 'none')
@@ -32,22 +90,77 @@ async function drawMap() {
   //   .attr('stroke', 'hsl(220, 72%, 68%)');
 
   const countries = topojson.feature(world, world.objects.countries);
-  bounds
+
+  worldGroup
     .append('g')
     .selectAll('path')
     .data(countries.features.filter(d => d.properties.name !== 'Antarctica'))
     .enter()
     .append('path')
     .attr('d', pathGenerator)
-    .attr('fill', 'hsl(206, 68%, 88%)')
+    .attr('fill', ({ id }) => {
+      if (dataMap.has(id)) {
+        const { weeks } = dataMap.get(id)[0];
+        return colors[groups.findIndex(({ threshold }) => weeks <= threshold)];
+      }
+      return colors[0];
+    })
     .attr('stroke-width', '0.1')
     .attr('stroke', 'hsl(214, 72%, 64%)')
-    .on('mouseenter', function() {
-      d3.select(this).attr('fill', 'hsl(40, 84%, 68%)');
+    .on('mouseenter', function(e, d) {
+      const { id } = d;
+      if (dataMap.has(id)) {
+        d3.select(this).style('filter', 'brightness(1.5)');
+        const { weeks, country } = dataMap.get(id)[0];
+
+        tooltip.append('h2').text(country);
+
+        tooltip.append('p').text(`${weeks} weeks`);
+
+        const [x, y] = pathGenerator.centroid(d);
+
+        tooltip
+          .style('transform', `translate(${x}px, ${y}px)`)
+          .style('opacity', 1);
+      }
     })
     .on('mouseleave', function() {
-      d3.select(this).attr('fill', 'hsl(206, 68%, 88%)');
+      d3.select(this).style('filter', 'brightness(1)');
+
+      tooltip
+        .style('opacity', 0)
+        .selectAll('*')
+        .remove();
     });
+
+  legendGroup.attr('transform', `translate(20 ${height / 1.5})`);
+
+  const legendGroups = legendGroup
+    .selectAll('g')
+    .data(groups)
+    .enter()
+    .append('g')
+    .attr(
+      'transform',
+      (d, i) => `translate(0 ${(height / 2.5 / (groups.length + 1)) * i})`
+    );
+
+  legendGroups
+    .append('circle')
+    .attr('r', 4)
+    .attr('fill', (d, i) => colors[i]);
+
+  legendGroups
+    .append('text')
+    .attr('dominant-baseline', 'middle')
+    .attr('x', 18)
+    .attr('font-size', 12)
+    .text(({ label }) => label);
+
+  root
+    .append('a')
+    .attr('href', 'https://en.unesco.org/covid19/educationresponse')
+    .text('Source');
 }
 
 drawMap();
