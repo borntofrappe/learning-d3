35bb09d47df79691634901687a0af5e9842d1097
borntofrappe <borntofrappe@gmail.com>
2021-11-16 23:34:11 +0100

    Geo Practice - Antipodes

--- Geo Practice/Antipodes/script.js
+++ Geo Practice/Antipodes/script.js
@@ -21,6 +21,18 @@ const pathGenerator = geoPath(
   projection().fitWidth(dimensions.boundedWidth, sphere)
 );
 
+const getPathGenerator = (rotate = [0, 0], reflectX = false) =>
+  reflectX
+    ? geoPath(
+        projection()
+          .fitWidth(dimensions.boundedWidth, sphere)
+          .rotate(rotate)
+          .reflectX([-1])
+      )
+    : geoPath(
+        projection().fitWidth(dimensions.boundedWidth, sphere).rotate(rotate)
+      );
+
 const y1 = pathGenerator.bounds(sphere)[1][1];
 
 dimensions.boundedHeight = y1;
@@ -30,20 +42,20 @@ dimensions.height =
 const main = d3.select("body").append("main");
 
 main.append("h1").text("Antipodes");
-main.append("p").text("Diametrical opposites");
 
-const input = main
-  .append("input")
-  .attr("type", "range")
-  .attr("min", "0")
-  .attr("max", "180")
-  .attr("value", "0");
+main
+  .append("p")
+  .text(
+    "There may not be antipathies on the opposite side of the Earth, but there is certainly a word for the concept."
+  );
 
 const wrapper = main
   .append("svg")
   .attr("viewBox", `0 0 ${dimensions.width} ${dimensions.height}`)
   .attr("width", dimensions.width)
   .attr("height", dimensions.height);
+
+const defs = wrapper.append("defs");
 
 const bounds = wrapper
   .append("g")
@@ -52,14 +64,16 @@ const bounds = wrapper
     `translate(${dimensions.margin.left} ${dimensions.margin.top})`
   );
 
-bounds.append("path").attr("d", pathGenerator(sphere)).attr("fill", "#75cdca");
+bounds
+  .append("path")
+  .attr("d", getPathGenerator()(sphere))
+  .attr("fill", "hsl(0, 0%, 97%)");
 
 bounds
   .append("path")
-  .attr("d", pathGenerator(graticule))
+  .attr("d", getPathGenerator()(graticule))
   .attr("fill", "none")
-  .attr("stroke", "currentColor")
-  .attr("stroke-width", "0.25");
+  .attr("stroke", "hsl(0, 0%, 84%)");
 
 bounds
   .append("text")
@@ -72,6 +86,12 @@ bounds
   .attr("y", dimensions.boundedHeight / 2)
   .attr("font-weight", "bold");
 
+// main
+//   .append("p")
+//   .append("a")
+//   .attr("href", "https://en.wikipedia.org/wiki/Antipodes")
+//   .text("Source");
+
 const timeout = setTimeout(() => {
   drawWorld();
 }, 500);
@@ -83,37 +103,31 @@ async function drawWorld() {
 
   const land = feature(world, world.objects.land);
 
+  defs
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("path")
+    .attr("d", getPathGenerator()(land));
+
   bounds.select("text").transition().attr("opacity", "0").remove();
 
   const worldGroup = bounds.append("g");
 
   worldGroup
     .append("path")
-    .attr(
-      "d",
-      geoPath(
-        projection().fitSize(
-          [dimensions.boundedWidth, dimensions.boundedHeight],
-          sphere
-        )
-      )(land)
-    )
-    .attr("fill", "blue")
-    .attr("opacity", "0.5");
+    .attr("d", getPathGenerator()(land))
+    .attr("fill", "hsl(217, 100%, 80%)");
 
   worldGroup
     .append("path")
-    .attr(
-      "d",
-      geoPath(
-        projection()
-          .fitSize([dimensions.boundedWidth, dimensions.boundedHeight], sphere)
-          .rotate([0, 180])
-          .reflectX([-1])
-      )(land)
-    )
-    .attr("fill", "red")
-    .attr("opacity", "0.5");
+    .attr("d", getPathGenerator([0, 180], true)(land))
+    .attr("fill", "hsl(51, 73%, 52%)");
+
+  worldGroup
+    .append("path")
+    .attr("clip-path", "url(#clip)")
+    .attr("d", getPathGenerator([0, 180], true)(land))
+    .attr("fill", "hsl(23, 74%, 50%)");
 
   worldGroup
     .attr("opacity", "0")
@@ -121,41 +135,36 @@ async function drawWorld() {
     .duration(1000)
     .attr("opacity", "1");
 
-  // input
-  //   .transition()
-  //   .duration(2000)
-  //   .attr("value", "180")
+  const input = main
+    .append("input")
+    .attr("type", "range")
+    .attr("min", "0")
+    .attr("max", "360")
+    .attr("value", "0");
+
+  // input.transition().delay(200).duration(2000).attr("value", "360");
 
   input.on("input", (e) => {
+    // input.interrupt();
+
     const angle = parseInt(e.target.value, 10);
+
+    defs.select("path").attr("d", getPathGenerator([angle, 0])(land));
+
+    bounds
+      .select("path:nth-of-type(2)")
+      .attr("d", getPathGenerator([angle, 0])(graticule));
 
     worldGroup
       .select("path:nth-of-type(1)")
-      .attr(
-        "d",
-        geoPath(
-          projection()
-            .fitSize(
-              [dimensions.boundedWidth, dimensions.boundedHeight],
-              sphere
-            )
-            .rotate([angle, 0])
-        )(land)
-      );
+      .attr("d", getPathGenerator([angle, 0])(land));
 
     worldGroup
       .select("path:nth-of-type(2)")
-      .attr(
-        "d",
-        geoPath(
-          projection()
-            .fitSize(
-              [dimensions.boundedWidth, dimensions.boundedHeight],
-              sphere
-            )
-            .rotate([angle, 180])
-            .reflectX([-1])
-        )(land)
-      );
+      .attr("d", getPathGenerator([angle, 180], true)(land));
+
+    worldGroup
+      .select("path:nth-of-type(3)")
+      .attr("d", getPathGenerator([angle, 180], true)(land));
   });
 }
