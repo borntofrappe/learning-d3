f0534a8469abb0ef8705871709aea288c8bf53f2
borntofrappe <borntofrappe@gmail.com>
2021-11-20 22:54:29 +0100

    static line charts

--- Seasonal Data/README.md
+++ Seasonal Data/README.md
@@ -5,3 +5,7 @@
 ## Notes
 
 [Google Trends](https://trends.google.com/trends/explore?q=sun,moon) highlights an interesting pattern in search interest for the keywords sun and moon and the last twelve months. The goal of this project is to immediately recreate the line chart and then repurpose the information with a small animation, playing on the following keywords: sunrise and sunset, moon phases.
+
+## Data
+
+Ultimately, I've chosen to focus on two separate arrays describing search interest for [sun](https://trends.google.com/trends/explore?q=sun) and [moon](https://trends.google.com/trends/explore?q=moon). I fear considering both has the effect of focusing the attention on the comparison more than the evolution of the individual variable.
--- Seasonal Data/script.js
+++ Seasonal Data/script.js
@@ -1,2 +1,539 @@
+const dataset = {
+  sun: [
+    {
+      date: "2020-11-22",
+      value: 75,
+    },
+    {
+      date: "2020-11-29",
+      value: 74,
+    },
+    {
+      date: "2020-12-06",
+      value: 76,
+    },
+    {
+      date: "2020-12-13",
+      value: 78,
+    },
+    {
+      date: "2020-12-20",
+      value: 76,
+    },
+    {
+      date: "2020-12-27",
+      value: 81,
+    },
+    {
+      date: "2021-01-03",
+      value: 79,
+    },
+    {
+      date: "2021-01-10",
+      value: 77,
+    },
+    {
+      date: "2021-01-17",
+      value: 79,
+    },
+    {
+      date: "2021-01-24",
+      value: 75,
+    },
+    {
+      date: "2021-01-31",
+      value: 80,
+    },
+    {
+      date: "2021-02-07",
+      value: 77,
+    },
+    {
+      date: "2021-02-14",
+      value: 78,
+    },
+    {
+      date: "2021-02-21",
+      value: 82,
+    },
+    {
+      date: "2021-02-28",
+      value: 80,
+    },
+    {
+      date: "2021-03-07",
+      value: 85,
+    },
+    {
+      date: "2021-03-14",
+      value: 81,
+    },
+    {
+      date: "2021-03-21",
+      value: 82,
+    },
+    {
+      date: "2021-03-28",
+      value: 85,
+    },
+    {
+      date: "2021-04-04",
+      value: 86,
+    },
+    {
+      date: "2021-04-11",
+      value: 86,
+    },
+    {
+      date: "2021-04-18",
+      value: 91,
+    },
+    {
+      date: "2021-04-25",
+      value: 89,
+    },
+    {
+      date: "2021-05-02",
+      value: 88,
+    },
+    {
+      date: "2021-05-09",
+      value: 89,
+    },
+    {
+      date: "2021-05-16",
+      value: 92,
+    },
+    {
+      date: "2021-05-23",
+      value: 95,
+    },
+    {
+      date: "2021-05-30",
+      value: 100,
+    },
+    {
+      date: "2021-06-06",
+      value: 99,
+    },
+    {
+      date: "2021-06-13",
+      value: 98,
+    },
+    {
+      date: "2021-06-20",
+      value: 91,
+    },
+    {
+      date: "2021-06-27",
+      value: 93,
+    },
+    {
+      date: "2021-07-04",
+      value: 89,
+    },
+    {
+      date: "2021-07-11",
+      value: 96,
+    },
+    {
+      date: "2021-07-18",
+      value: 98,
+    },
+    {
+      date: "2021-07-25",
+      value: 87,
+    },
+    {
+      date: "2021-08-01",
+      value: 82,
+    },
+    {
+      date: "2021-08-08",
+      value: 86,
+    },
+    {
+      date: "2021-08-15",
+      value: 82,
+    },
+    {
+      date: "2021-08-22",
+      value: 80,
+    },
+    {
+      date: "2021-08-29",
+      value: 78,
+    },
+    {
+      date: "2021-09-05",
+      value: 82,
+    },
+    {
+      date: "2021-09-12",
+      value: 78,
+    },
+    {
+      date: "2021-09-19",
+      value: 78,
+    },
+    {
+      date: "2021-09-26",
+      value: 77,
+    },
+    {
+      date: "2021-10-03",
+      value: 78,
+    },
+    {
+      date: "2021-10-10",
+      value: 76,
+    },
+    {
+      date: "2021-10-17",
+      value: 75,
+    },
+    {
+      date: "2021-10-24",
+      value: 77,
+    },
+    {
+      date: "2021-10-31",
+      value: 72,
+    },
+    {
+      date: "2021-11-07",
+      value: 76,
+    },
+    {
+      date: "2021-11-14",
+      value: 74,
+    },
+  ],
+  moon: [
+    {
+      date: "2020-11-22",
+      value: 53,
+    },
+    {
+      date: "2020-11-29",
+      value: 60,
+    },
+    {
+      date: "2020-12-06",
+      value: 51,
+    },
+    {
+      date: "2020-12-13",
+      value: 51,
+    },
+    {
+      date: "2020-12-20",
+      value: 52,
+    },
+    {
+      date: "2020-12-27",
+      value: 72,
+    },
+    {
+      date: "2021-01-03",
+      value: 54,
+    },
+    {
+      date: "2021-01-10",
+      value: 55,
+    },
+    {
+      date: "2021-01-17",
+      value: 53,
+    },
+    {
+      date: "2021-01-24",
+      value: 65,
+    },
+    {
+      date: "2021-01-31",
+      value: 54,
+    },
+    {
+      date: "2021-02-07",
+      value: 53,
+    },
+    {
+      date: "2021-02-14",
+      value: 54,
+    },
+    {
+      date: "2021-02-21",
+      value: 68,
+    },
+    {
+      date: "2021-02-28",
+      value: 60,
+    },
+    {
+      date: "2021-03-07",
+      value: 55,
+    },
+    {
+      date: "2021-03-14",
+      value: 54,
+    },
+    {
+      date: "2021-03-21",
+      value: 60,
+    },
+    {
+      date: "2021-03-28",
+      value: 66,
+    },
+    {
+      date: "2021-04-04",
+      value: 53,
+    },
+    {
+      date: "2021-04-11",
+      value: 60,
+    },
+    {
+      date: "2021-04-18",
+      value: 67,
+    },
+    {
+      date: "2021-04-25",
+      value: 99,
+    },
+    {
+      date: "2021-05-02",
+      value: 54,
+    },
+    {
+      date: "2021-05-09",
+      value: 64,
+    },
+    {
+      date: "2021-05-16",
+      value: 55,
+    },
+    {
+      date: "2021-05-23",
+      value: 100,
+    },
+    {
+      date: "2021-05-30",
+      value: 53,
+    },
+    {
+      date: "2021-06-06",
+      value: 53,
+    },
+    {
+      date: "2021-06-13",
+      value: 51,
+    },
+    {
+      date: "2021-06-20",
+      value: 71,
+    },
+    {
+      date: "2021-06-27",
+      value: 51,
+    },
+    {
+      date: "2021-07-04",
+      value: 51,
+    },
+    {
+      date: "2021-07-11",
+      value: 54,
+    },
+    {
+      date: "2021-07-18",
+      value: 76,
+    },
+    {
+      date: "2021-07-25",
+      value: 55,
+    },
+    {
+      date: "2021-08-01",
+      value: 50,
+    },
+    {
+      date: "2021-08-08",
+      value: 52,
+    },
+    {
+      date: "2021-08-15",
+      value: 56,
+    },
+    {
+      date: "2021-08-22",
+      value: 65,
+    },
+    {
+      date: "2021-08-29",
+      value: 50,
+    },
+    {
+      date: "2021-09-05",
+      value: 50,
+    },
+    {
+      date: "2021-09-12",
+      value: 54,
+    },
+    {
+      date: "2021-09-19",
+      value: 71,
+    },
+    {
+      date: "2021-09-26",
+      value: 47,
+    },
+    {
+      date: "2021-10-03",
+      value: 51,
+    },
+    {
+      date: "2021-10-10",
+      value: 50,
+    },
+    {
+      date: "2021-10-17",
+      value: 70,
+    },
+    {
+      date: "2021-10-24",
+      value: 62,
+    },
+    {
+      date: "2021-10-31",
+      value: 48,
+    },
+    {
+      date: "2021-11-07",
+      value: 53,
+    },
+    {
+      date: "2021-11-14",
+      value: 66,
+    },
+  ],
+};
+
+const {
+  select,
+  extent,
+  max,
+  scaleLinear,
+  scaleTime,
+  axisLeft,
+  axisBottom,
+  timeParse,
+  timeFormat,
+  line,
+} = d3;
+
 const main = d3.select("body").append("main");
 main.append("h1").text("Seasonal Data");
+main
+  .append("p")
+  .html(
+    "Google Trends highlights an interesting pattern in search interest for the keywords <a href='https://trends.google.com/trends/explore?q=sun'>sun</a> and <a href='https://trends.google.com/trends/explore?q=moon'>moon</a>."
+  );
+
+function drawLinechart(key) {
+  const data = dataset[key];
+
+  const dimensions = {
+    width: 800,
+    height: 200,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 30,
+      left: 30,
+    },
+  };
+
+  dimensions.boundedWidth =
+    dimensions.width - (dimensions.margin.left + dimensions.margin.right);
+  dimensions.boundedHeight =
+    dimensions.height - (dimensions.margin.top + dimensions.margin.bottom);
+
+  const parseDate = timeParse("%Y-%m-%d");
+  const formatDate = timeFormat("%B %d, %Y");
+
+  const xAccessor = (d) => parseDate(d.date);
+  const yAccessor = (d) => d.value;
+
+  const xScale = scaleTime()
+    .domain(extent(data, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice();
+
+  const yScale = scaleLinear()
+    .domain([0, 100])
+    .range([dimensions.boundedHeight, 0]);
+
+  const lineGenerator = line()
+    .x((d) => xScale(xAccessor(d)))
+    .y((d) => yScale(yAccessor(d)));
+
+  const xAxis = axisBottom(xScale)
+    .ticks(5)
+    .tickSize(0)
+    .tickFormat((d) => formatDate(d));
+
+  const yAxis = axisLeft(yScale).ticks(4).tickSize(0);
+
+  const article = main.append("article");
+  article.append("h2").text(`Interest over time: ${key}`);
+
+  const wrapper = article
+    .append("svg")
+    .attr("viewBox", `0 0 ${dimensions.width} ${dimensions.height}`)
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .attr(
+      "transform",
+      `translate(${dimensions.margin.left} ${dimensions.margin.top})`
+    );
+
+  const dataGroup = bounds.append("g");
+  const axisGroup = bounds.append("g");
+
+  dataGroup
+    .append("path")
+    .attr("d", lineGenerator(data))
+    .attr("fill", "none")
+    .attr("stroke", "hsl(207, 90%, 54%)")
+    .attr("stroke-width", "3");
+
+  const xAxisGroup = axisGroup
+    .append("g")
+    .attr("transform", `translate(0 ${dimensions.boundedHeight})`)
+    .call(xAxis);
+
+  const yAxisGroup = axisGroup.append("g").call(yAxis);
+
+  axisGroup.selectAll("text").attr("fill", "hsl(0, 0%, 58%)");
+
+  yAxisGroup.select("path").remove();
+  yAxisGroup.select("g.tick").remove();
+  yAxisGroup
+    .selectAll("g.tick")
+    .append("path")
+    .attr("d", `M 0 0 h ${dimensions.boundedWidth}`)
+    .attr("stroke-width", "0.5")
+    .attr("stroke", "hsl(0, 0%, 84%)");
+
+  xAxisGroup.select("path").attr("stroke", "hsl(0, 0%, 68%)");
+  xAxisGroup.selectAll("text").attr("y", "6");
+}
+
+drawLinechart("sun");
+drawLinechart("moon");
