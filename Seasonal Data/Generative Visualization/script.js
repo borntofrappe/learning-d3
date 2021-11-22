1f17ddef5510cfbab2a62f6599eaf07f6e06af76
borntofrappe <borntofrappe@gmail.com>
2021-11-22 08:56:35 +0100

    working generative viz

--- Seasonal Data/Generative Visualization/script.js
+++ Seasonal Data/Generative Visualization/script.js
@@ -419,30 +419,165 @@ const dataset = {
       value: 66,
     },
   ],
-};
+}
 
-const { select } = d3;
+const icons = {
+  sun: `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100" width="200" height="200">
+      <defs>
+        <linearGradient id="gs" x1="-50" x2="50" y1="-50" y2="50">
+          <stop stop-color="hsl(0, 0%, 0%)" offset="0" />
+          <stop stop-color="hsl(0, 0%, 100%)" offset="1" />
+        </linearGradient>
+        <mask id="ms">
+          <rect x="-50" y="-50" width="100" height="100" fill="url(#gs)" />
+        </mask>
+      </defs>
 
-const main = d3.select("body").append("main");
-main.append("h1").text("Seasonal Data");
+      <rect x="-30" y="-30" width="60" height="60" rx="5" fill="hsl(53, 97%, 53%)" />
+      <rect x="-30" y="-30" width="60" height="60" rx="5" fill="hsl(45, 98%, 51%)" mask="url(#ms)" />
+      <rect x="-30" y="-30" width="60" height="60" rx="5" transform="rotate(45)" fill="hsl(53, 97%, 53%)" />
+      <rect x="-30" y="-30" width="60" height="60" rx="5" transform="rotate(45)" fill="hsl(45, 98%, 51%)" mask="url(#ms)" />
+    </svg>
+  `,
+  moon: `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-50 -50 100 100" width="200" height="200">
+    <defs>
+      <linearGradient id="gm" x1="-50" x2="50" y1="-50" y2="50">
+        <stop stop-color="hsl(0, 0%, 0%)" offset="0" />
+        <stop stop-color="hsl(0, 0%, 100%)" offset="1" />
+      </linearGradient>
+      <mask id="mm">
+        <rect x="-50" y="-50" width="100" height="100" fill="url(#gm)" />
+      </mask>
+    </defs>
+
+    <g transform="translate(-5 0) rotate(-20)" stroke-width="10" stroke-linecap="round" stroke-linejoin="round">
+      <path fill="hsl(244, 97%, 53%)" stroke="hsl(244, 97%, 53%)" d="M 20 -25 A 30 30 0 1 0 20 25 32.5 32.5 0 0 1 20 -25" />
+      <path fill="hsl(242, 98%, 50%)" stroke="hsl(242, 98%, 50%)" mask="url(#mm)" d="M 20 -25 A 30 30 0 1 0 20 25 32.5 32.5 0 0 1 20 -25" />
+    </g>
+  </svg>
+`,
+}
+
+const { select, scaleLinear, timeParse, timeFormat } = d3
+
+const main = d3.select("body").append("main")
+main.append("h1").text("Seasonal Data")
 main
   .append("p")
   .html(
     "<time datetime='2021-11-20'>Over the span of twelve months</time> Google Trends highlights an interesting pattern for the keywords <a href='https://trends.google.com/trends/explore?q=sun'>sun</a> and <a href='https://trends.google.com/trends/explore?q=moon'>moon</a>."
-  );
+  )
 
-const articleSun = main.append("article");
-articleSun.append("h2").text("Sun");
-articleSun
-  .append("p")
-  .text(
-    "Search interest steadily grows and then recedes without changing excessively."
-  );
+function draw({ title, description, key }) {
+  const data = dataset[key]
 
-// const articleMoon = main.append("article");
-// articleMoon.append("h2").text("Moon");
-// articleMoon
-//   .append("p")
-//   .text(
-//     "Search interest fluctuates continuously in smaller, month-long periods."
-//   );
+  const parseDate = timeParse("%Y-%m-%d")
+  const formatDate = timeFormat("%B %-d, %Y")
+
+  const xAccessor = (d) => parseDate(d.date)
+  const yAccessor = (d) => d.value
+
+  const yScale = scaleLinear().domain([0, 100]).range([180, 0])
+
+  const article = main.append("article")
+  article.append("h2").text(title).style("text-transform", "capitalize")
+  article.append("p").text(description)
+
+  const wrapper = article
+    .append("svg")
+    .attr("viewBox", `0 0 250 500`)
+    .attr("width", 250)
+    .attr("height", 500)
+
+  const bounds = wrapper.append("g").attr("transform", `translate(25 25)`)
+
+  const iconGroup = bounds.append("g")
+  const lineGroup = bounds.append("g").attr("transform", "translate(0 380)")
+  const textGroup = bounds.append("g").attr("transform", "translate(200 420)")
+
+  iconGroup.append("g").html(icons[key])
+
+  textGroup
+    .attr("text-anchor", "end")
+    .attr("fill", "currentColor")
+    .attr("font-family", "inherit")
+
+  textGroup.append("text").attr("font-size", "56").attr("font-weight", "bold")
+
+  textGroup.append("text").attr("y", "30").attr("font-size", "22")
+
+  iconGroup.attr("transform", `translate(0 ${yScale(yAccessor(data[0]))})`)
+
+  textGroup.select("text").text(yAccessor(data[0]))
+
+  textGroup.select("text:nth-of-type(2)").text(formatDate(xAccessor(data[0])))
+
+  const button = article.append("button").text("Run animation")
+
+  const input = article
+    .append("input")
+    .attr("type", "range")
+    .attr("min", 0)
+    .attr("max", data.length - 1)
+    .attr("value", 0)
+
+  let transition
+  function animate(value = 0) {
+    transition = d3.transition().duration(250).ease(d3.easeLinear)
+    textGroup.select("text").text(yAccessor(data[value]))
+
+    textGroup
+      .select("text:nth-of-type(2)")
+      .text(formatDate(xAccessor(data[value])))
+
+    iconGroup
+      .transition(transition)
+      .attr("transform", `translate(0 ${yScale(yAccessor(data[value]))})`)
+      .on("end", () => {
+        if (value < data.length - 1) animate(value + 1)
+      })
+
+    input.attr("value", value)
+  }
+
+  button.on("click", () => {
+    animate()
+  })
+
+  input
+    .on("input", (e) => {
+      iconGroup.interrupt()
+
+      const value = parseInt(e.currentTarget.value, 10)
+
+      textGroup.select("text").text(yAccessor(data[value]))
+
+      textGroup
+        .select("text:nth-of-type(2)")
+        .text(formatDate(xAccessor(data[value])))
+    })
+    .on("change", (e) => {
+      const value = parseInt(e.currentTarget.value, 10)
+
+      // if run on input I feel the transition is run too frequently and changes are less apparent
+      iconGroup
+        .transition()
+        .attr("transform", `translate(0 ${yScale(yAccessor(data[value]))})`)
+    })
+}
+
+draw({
+  title: "sun",
+  description:
+    "Search interest steadily grows and then recedes without changing excessively.",
+  key: "sun",
+})
+
+draw({
+  title: "moon",
+  description:
+    "Search interest fluctuates continuously in smaller, month-long periods.",
+  key: "moon",
+})
