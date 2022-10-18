a6ed4a85aa85a98f97f098bd7d6447989463ed78
Gabriele Corti <borntofrappe@gmail.com>
2022-10-18 18:57:59 +0200

    draw continental france

--- Beauteous Villages/README.md
+++ Beauteous Villages/README.md
@@ -18,3 +18,51 @@ filter 'type_en == "Metropolitan department"'
 ```
 
 Export to GeoJSON — see `france.json`.
+
+### Projection
+
+The coordinates are already projected, so it would be possible to use the features directly.
+
+```js
+const path = d3
+  .geoPath()
+
+  // later
+  .append("path")
+  .attr("d", path);
+```
+
+There are however two issues.
+
+1. you need to draw the departments in an SVG width a given width and height (or in this instance `viewBox`)
+
+   Use an identity projection and the `fitSize` method. As a simplification I chose to map the data in a squared canvas
+
+   ```js
+   const projection = d3.geoIdentity().fitSize([size, size], json);
+   ```
+
+2. the map is upside down
+
+   Flip the vertical coordinate, again through the projection, with the `reflectY` method
+
+   ```js
+   const projection = d3
+     .geoIdentity()
+     .reflectY(true)
+     .fitSize([size, size], json);
+   ```
+
+Apply the projection to the path.
+
+```js
+const path = d3.geoPath().projection(projection);
+```
+
+### Drawing
+
+You need to load the JSON data with `d3.json`, an asynchronous function. To avoid a layout shift immediately add the `<svg>` element and then, _when the data is vailable_, draw the necessary visuals.
+
+It would be possible to conjure something more complex, like an animated loader, but in the demo I was satisfied to add a string of text with a temporary message.
+
+With a D3 transition you can remove the default view and introduce the map smoothly over time — in the demo with a fading transition.
--- Beauteous Villages/script.js
+++ Beauteous Villages/script.js
@@ -1,2 +1,63 @@
-const main = d3.select("body").append("main");
-main.append("h1").text("Beauteous Villages");
+const div = d3.select("body").append("div");
+
+const header = div.append("header");
+header.append("h1").text("Beauteous Villages");
+header
+  .append("p")
+  .html("Let us focus on small, beautiful villages in continental France");
+
+const size = 600;
+
+const svg = div
+  .append("svg")
+  .attr("viewBox", `0 0 ${size} ${size}`)
+  .style("max-width", "40rem");
+
+svg
+  .append("g")
+  .attr("transform", `translate(${size / 2} ${size / 2})`)
+  .attr("text-anchor", "middle")
+  .attr("dominant-baseline", "central")
+  .append("text")
+  .text("Loading Geographical Data");
+
+// REQUIRE A SERVER TO BYPASS CORS
+(async () => {
+  const json = await d3.json("france.json");
+
+  const projection = d3
+    .geoIdentity()
+    .reflectY(true)
+    .fitSize([size, size], json);
+
+  const path = d3.geoPath().projection(projection);
+
+  svg
+    .select("g")
+    .attr("opacity", "1")
+    .transition()
+    .delay(500)
+    .duration(500)
+    .ease(d3.easeQuadOut)
+    .attr("opacity", "0")
+    .remove()
+    .on("end", () => {
+      const group = svg.append("g");
+
+      group
+        .attr("opacity", "0")
+        .transition()
+        .duration(500)
+        .ease(d3.easeQuadIn)
+        .attr("opacity", "1");
+
+      group
+        .selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "hsl(0, 0%, 37%)")
+        .attr("stroke", "hsl(0, 0%, 97%)");
+    });
+})();
