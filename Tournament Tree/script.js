92f58cedf6e3d55d5f45267931e33edd9e96cc93
Gabriele Corti <borntofrappe@gmail.com>
2022-11-05 22:35:17 +0100

    add tree layout

--- Tournament Tree/script.js
+++ Tournament Tree/script.js
@@ -1,30 +1,87 @@
-const main = d3.select("body").append("main");
-main.append("h1").text("Tournament Tree");
-
 const data = {
-  players: ["Djokovic", "Rune"],
-  brackets: [
+  name: ["Djokovic", "Rune"],
+  children: [
     {
-      players: ["Rune", "Auger-Aliassime"],
-      brackets: [
+      name: ["Rune", "Auger-Aliassime"],
+      children: [
         {
-          players: ["Alcaraz", "Rune"],
+          name: ["Alcaraz", "Rune"],
         },
         {
-          players: ["Tiafoe", "Auger-Aliassime"],
+          name: ["Tiafoe", "Auger-Aliassime"],
         },
       ],
     },
     {
-      players: ["Djokovic", "Tsisipas"],
-      brackets: [
+      name: ["Djokovic", "Tsisipas"],
+      children: [
         {
-          players: ["Djokovic", "Musetti"],
+          name: ["Djokovic", "Musetti"],
         },
         {
-          players: ["Tsisipas", "Paul"],
+          name: ["Tsisipas", "Paul"],
         },
       ],
     },
   ],
 };
+
+const width = 600;
+const height = 400;
+const margin = {
+  top: 0,
+  right: 200,
+  bottom: 0,
+  left: 10,
+};
+
+const root = d3.hierarchy(data);
+const dataTree = d3.tree().size([height, width])(root);
+
+const link = d3
+  .link(d3.curveStep)
+  .x((d) => d.y)
+  .y((d) => d.x);
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr(
+    "viewBox",
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  );
+
+const group = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left} ${margin.top})`);
+
+const groupLinks = group.append("g");
+const groupNodes = group.append("g");
+
+groupLinks
+  .selectAll("path")
+  .data(dataTree.links())
+  .enter()
+  .append("path")
+  .attr("fill", "none")
+  .attr("stroke", "currentColor")
+  .attr("stroke-width", "1")
+  .attr("d", link);
+
+const groupsNodes = groupNodes
+  .selectAll("g")
+  .data(dataTree.descendants())
+  .enter()
+  .append("g")
+  .attr("fill", "currentColor")
+  .attr("transform", ({ x, y }) => `translate(${y} ${x})`);
+
+groupsNodes.append("circle").attr("r", "5");
+groupsNodes
+  .append("text")
+  .attr("r", "5")
+  .attr("x", "5")
+  .attr("y", "-7")
+  .text(({ data }) => data.name);
