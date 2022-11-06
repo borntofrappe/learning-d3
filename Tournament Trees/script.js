06403ec3183480ac890974438e51e01a15ee82c4
Gabriele Corti <borntofrappe@gmail.com>
2022-11-06 21:17:51 +0100

    draw tournament tree

--- Tournament Trees/script.js
+++ Tournament Trees/script.js
@@ -37,66 +37,81 @@ const brackets = {
   ],
 };
 
-const width = 400;
-const height = 400;
-const padding = 20;
+(() => {
+  const bracket = brackets.top;
 
-const dataIds = [{ id: 0, parentId: null }];
-const depth = 3;
-let id = 0;
-let parentId = 0;
-for (let i = 0; i < 2 ** depth - 1; i++) {
-  dataIds.push({ id: ++id, parentId });
-  dataIds.push({ id: ++id, parentId });
-  parentId++;
-}
+  const width = 400;
+  const height = 400;
+  const padding = 20;
 
-const root = d3
-  .stratify()
-  .id((d) => d.id)
-  .parentId((d) => d.parentId)(dataIds);
+  const fontSize = 6;
+  const gap = 2;
 
-const dataTree = d3.tree().size([width, height])(root);
+  const dataIds = [{ id: 0, parentId: null }];
+  const depth = bracket.length ** 0.5;
+  let id = 0;
+  let parentId = 0;
+  for (let i = 0; i < 2 ** depth - 1; i++) {
+    dataIds.push({ id: ++id, parentId });
+    dataIds.push({ id: ++id, parentId });
+    parentId++;
+  }
 
-const link = d3
-  .linkVertical()
-  .x((d) => d.x)
-  .y((d) => d.y);
+  const root = d3
+    .stratify()
+    .id((d) => d.id)
+    .parentId((d) => d.parentId)(dataIds);
 
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr(
-    "viewBox",
-    `${padding * -1} ${padding * -1} ${width + padding * 2} ${
-      height + padding * 2
-    }`
-  );
+  const dataTree = d3.tree().size([width, height])(root);
 
-const groupLinks = svg.append("g");
-const groupData = svg.append("g");
+  const link = d3
+    .link(d3.curveStep)
+    .x((d) => width - d.y)
+    .y((d) => d.x);
 
-groupLinks
-  .attr("fill", "none")
-  .attr("stroke", "currentColor")
-  .attr("stroke-width", "2");
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr(
+      "viewBox",
+      `${padding * -1} ${padding * -1} ${width + padding * 2} ${
+        height + padding * 2
+      }`
+    );
 
-groupData
-  .attr("font-size", "12")
-  .attr("font-size", "currentColor")
-  .attr("font-family", "sans-serif");
+  const groupLinks = svg.append("g");
+  const groupText = svg.append("g");
 
-groupData
-  .selectAll("circle")
-  .data(dataTree.descendants())
-  .enter()
-  .append("circle")
-  .attr("transform", (d) => `translate(${d.x} ${d.y})`)
-  .attr("r", "4");
+  groupLinks
+    .attr("fill", "none")
+    .attr("stroke", "currentColor")
+    .attr("stroke-width", "0.5");
 
-groupLinks
-  .selectAll("path")
-  .data(dataTree.links())
-  .enter()
-  .append("path")
-  .attr("d", link);
+  groupText
+    .attr("font-size", fontSize)
+    .attr("fill", "currentColor")
+    .attr("font-family", "sans-serif");
+
+  const text = groupText
+    .selectAll("text")
+    .data(dataTree.leaves())
+    .enter()
+    .append("text")
+    .attr("transform", (d) => `translate(${width - d.y} ${d.x - 1 - gap / 2})`);
+
+  text
+    .selectAll("tspan")
+    .data((_, i) => bracket[i])
+    .enter()
+    .append("tspan")
+    .attr("x", "0")
+    .attr("dy", (_, i) => i * (fontSize + gap))
+    .text((d) => d);
+
+  groupLinks
+    .selectAll("path")
+    .data(dataTree.links())
+    .enter()
+    .append("path")
+    .attr("d", link);
+})();
