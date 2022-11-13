17450498515e62727975dad3350b09b7efc2d265
Gabriele Corti <borntofrappe@gmail.com>
2022-11-13 21:51:02 +0100

    add chart function
    recreate the scatterplot with a function, which is then called on a selection

--- Reusable Chart/script.js
+++ Reusable Chart/script.js
@@ -53,16 +53,7 @@ const datasets = [
   ],
 ];
 
-const domainX = [
-  0,
-  d3.max(datasets, (dataset) => d3.max(dataset, (d) => d[0])),
-];
-const domainY = [
-  0,
-  d3.max(datasets, (dataset) => d3.max(dataset, (d) => d[1])),
-];
-
-const [dataset] = datasets;
+const [, dataset] = datasets;
 
 const width = 650;
 const height = 400;
@@ -73,12 +64,14 @@ const margin = {
   right: 5,
 };
 
-const scaleX = d3.scaleLinear().domain(domainX).range([0, width]).nice();
-
-const scaleY = d3.scaleLinear().domain(domainY).range([height, 0]).nice();
-
-const axisX = d3.axisBottom(scaleX).ticks(8).tickSizeOuter(0);
-const axisY = d3.axisLeft(scaleY).ticks(6).tickSizeOuter(0);
+const domainX = [
+  0,
+  d3.max(datasets, (dataset) => d3.max(dataset, (d) => d[0])),
+];
+const domainY = [
+  0,
+  d3.max(datasets, (dataset) => d3.max(dataset, (d) => d[1])),
+];
 
 const svg = d3
   .select("body")
@@ -94,78 +87,152 @@ const group = svg
   .append("g")
   .attr("transform", `translate(${margin.left} ${margin.top})`);
 
-const groupAxis = group.append("g");
-const groupAxisLabels = groupAxis.append("g");
+group.call(
+  chart()
+    .width(width)
+    .height(height)
+    .data(dataset)
+    .margin(margin)
+    .domainX(domainX)
+    .domainY(domainY)
+);
 
-const groupDataset = group.append("g");
+function chart() {
+  let width;
+  let height;
+  let margin;
+  let data;
+  let domainX;
+  let domainY;
 
-groupAxis
-  .append("rect")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("fill", "none")
-  .attr("stroke", "currentColor")
-  .attr("stroke-width", "1");
+  function my(selection) {
+    const scaleX = d3.scaleLinear().domain(domainX).range([0, width]).nice();
+    const scaleY = d3.scaleLinear().domain(domainY).range([height, 0]).nice();
 
-groupAxis
-  .append("g")
-  .attr("class", "axis x-axis")
-  .call(axisX)
-  .attr("transform", `translate(0 ${height})`);
+    const axisX = d3.axisBottom(scaleX).ticks(8).tickSizeOuter(0);
+    const axisY = d3.axisLeft(scaleY).ticks(6).tickSizeOuter(0);
 
-groupAxis.append("g").attr("class", "axis y-axis").call(axisY);
+    const groupAxis = selection.append("g");
+    const groupAxisLabels = groupAxis.append("g");
 
-groupAxis.selectAll(".axis").select("path").remove();
-groupAxis.selectAll(".axis").select(".tick").remove();
-groupAxis.selectAll(".axis").select(".tick:last-of-type").remove();
-groupAxis.selectAll("text").attr("font-size", "15");
+    const groupDataset = selection.append("g");
 
-groupAxisLabels
-  .attr("stroke", "currentColor")
-  .attr("font-size", "18")
-  .style("text-transform", "uppercase");
+    groupAxis
+      .append("rect")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("fill", "none")
+      .attr("stroke", "currentColor")
+      .attr("stroke-width", "1");
 
-groupAxisLabels
-  .append("text")
-  .attr("transform", `translate(${width / 2} ${height + margin.bottom - 1})`)
-  .attr("text-anchor", "middle")
-  .text("x");
+    groupAxis
+      .append("g")
+      .attr("class", "axis x-axis")
+      .call(axisX)
+      .attr("transform", `translate(0 ${height})`);
 
-groupAxisLabels
-  .append("text")
-  .attr("text-anchor", "middle")
-  .attr("dominant-baseline", "hanging")
-  .attr("transform", `translate(${-margin.left} ${height / 2}) rotate(-90)`)
-  .text("y");
+    groupAxis.append("g").attr("class", "axis y-axis").call(axisY);
 
-const groupsDataset = groupDataset
-  .selectAll("circle")
-  .data(dataset)
-  .enter()
-  .append("circle")
-  .attr("transform", ([x, y]) => `translate(${scaleX(x)} ${scaleY(y)})`)
-  .attr("r", "7")
-  .attr("fill", "var(--color-data, currentColor)")
-  .attr("fill-opacity", "0.3")
-  .attr("stroke", "var(--color-data, currentColor)")
-  .attr("stroke-width", "1");
+    groupAxis.selectAll(".axis").select("path").remove();
+    groupAxis.selectAll(".axis").select(".tick").remove();
+    groupAxis.selectAll(".axis").select(".tick:last-of-type").remove();
+    groupAxis.selectAll("text").attr("font-size", "15");
 
-groupDataset
-  .append("path")
-  .datum(() => {})
-  .attr("d", () => {
-    const { length: n } = dataset;
-    const sumX = dataset.reduce((a, c) => a + c[0], 0);
-    const sumY = dataset.reduce((a, c) => a + c[1], 0);
-    const sumXY = dataset.reduce((a, c) => a + c[0] * c[1], 0);
-    const sumX2 = dataset.reduce((a, c) => a + c[0] ** 2, 0);
+    groupAxisLabels
+      .attr("stroke", "currentColor")
+      .attr("font-size", "18")
+      .style("text-transform", "uppercase");
 
-    const a = (sumY * sumX2 - sumX * sumXY) / (n * sumX2 - sumX ** 2);
-    const b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX ** 2);
+    groupAxisLabels
+      .append("text")
+      .attr(
+        "transform",
+        `translate(${width / 2} ${height + margin.bottom - 1})`
+      )
+      .attr("text-anchor", "middle")
+      .text("x");
 
-    return `M ${[0, width]
-      .map((x) => [x, scaleY(a + b * scaleX.invert(x))].join(" "))
-      .join(" ")}`;
-  })
-  .attr("stroke", "currentColor")
-  .attr("stroke-width", "1");
+    groupAxisLabels
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "hanging")
+      .attr("transform", `translate(${-margin.left} ${height / 2}) rotate(-90)`)
+      .text("y");
+
+    const groupsDataset = groupDataset
+      .selectAll("circle")
+      .data(dataset)
+      .enter()
+      .append("circle")
+      .attr("transform", ([x, y]) => `translate(${scaleX(x)} ${scaleY(y)})`)
+      .attr("r", "7")
+      .attr("fill", "var(--color-data, currentColor)")
+      .attr("fill-opacity", "0.3")
+      .attr("stroke", "var(--color-data, currentColor)")
+      .attr("stroke-width", "1");
+
+    groupDataset
+      .append("path")
+      .datum(() => {})
+      .attr("d", () => {
+        const { length: n } = dataset;
+        const sumX = dataset.reduce((a, c) => a + c[0], 0);
+        const sumY = dataset.reduce((a, c) => a + c[1], 0);
+        const sumXY = dataset.reduce((a, c) => a + c[0] * c[1], 0);
+        const sumX2 = dataset.reduce((a, c) => a + c[0] ** 2, 0);
+
+        const a = (sumY * sumX2 - sumX * sumXY) / (n * sumX2 - sumX ** 2);
+        const b = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX ** 2);
+
+        return `M ${[0, width]
+          .map((x) => [x, scaleY(a + b * scaleX.invert(x))].join(" "))
+          .join(" ")}`;
+      })
+      .attr("stroke", "currentColor")
+      .attr("stroke-width", "1");
+  }
+
+  my.width = function (value) {
+    if (!arguments.length) return width;
+
+    width = value;
+    return my;
+  };
+
+  my.height = function (value) {
+    if (!arguments.length) return height;
+
+    height = value;
+    return my;
+  };
+
+  my.margin = function (value) {
+    if (!arguments.length) return margin;
+
+    margin = value;
+    return my;
+  };
+
+  my.data = function (value) {
+    if (!arguments.length) return data;
+
+    data = value;
+    return my;
+  };
+
+  my.domainX = function (value) {
+    if (!arguments.length) return domainX;
+
+    domainX = value;
+    return my;
+  };
+
+  my.domainY = function (value) {
+    if (!arguments.length) return domainY;
+
+    domainY = value;
+    return my;
+  };
+
+  return my;
+}
