78297fe1e7910342c5700263b39afe5d74937c58
Gabriele Corti <borntofrappe@gmail.com>
2022-11-24 21:29:40 +0100

    first commit #31

---
+++ Proportional Area Chart/README.md
@@ -0,0 +1,19 @@
+# Proportional Area Chart
+
+Recreate one of the examples from [datavizproject](https://datavizproject.com/data-type/proportional-area-chart-half-circle/) for a _Proportional Area Chart (Half Circle)_.
+
+## Data
+
+The script includes sales figures from bulbapedia. To have some kind of comparison between values I focused on figures for the units sold in Japan and the first week. Each data point includes units sold and _sell-through_ rate, meaning the percentage sold by retailers out of units shipped.
+
+- [Ruby and Sapphire](https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_Ruby_and_Sapphire_Versions#Sales)
+
+- [Diamond and Pearl](https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_Diamond_and_Pearl_Versions#Reception)
+
+- [Black and White](https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_Black_and_White_Versions#Sales)
+
+- [X and Y](https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_X_and_Y#Reception)
+
+- [Sun and Moon](https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_Sun_and_Moon#Reception)
+
+- [Sword and Shield](https://bulbapedia.bulbagarden.net/wiki/Pok%C3%A9mon_Sword_and_Shield#Reception)
---
+++ Proportional Area Chart/script.js
@@ -0,0 +1,50 @@
+const data = [
+  {
+    title: "Ruby and Sapphire",
+    sales: 1093299,
+    breakdown: [
+      { title: "Ruby", sales: 543962, "sell-through": 89.79 },
+      { title: "Sapphire", sales: 549337, "sell-through": 90.11 },
+    ],
+  },
+  {
+    title: "Diamond and Pearl",
+    sales: 1588734,
+    breakdown: [
+      { title: "Diamond", sales: 820047, "sell-through": 97.12 },
+      { title: "Pearl", sales: 768687, "sell-through": 96.16 },
+    ],
+  },
+  {
+    title: "Black and White",
+    sales: 2557779,
+    breakdown: [
+      { title: "Black", sales: 1323423, "sell-through": 89.21 },
+      { title: "White", sales: 1234356, "sell-through": 86.09 },
+    ],
+  },
+  {
+    title: "X and Y",
+    sales: 1866570,
+    breakdown: [
+      { title: "X", sales: 961003, "sell-through": 83.3 },
+      { title: "Y", sales: 905567, "sell-through": 78.23 },
+    ],
+  },
+  {
+    title: "Sun and Moon",
+    sales: 1590629,
+    breakdown: [
+      { title: "Sun", sales: 807540, "sell-through": 81.9 },
+      { title: "Moon", sales: 783090, "sell-through": 79.83 },
+    ],
+  },
+  {
+    title: "Sword and Shield",
+    sales: 894123,
+    breakdown: [
+      { title: "Sword", sales: 534306, "sell-through": 84.96 },
+      { title: "Shield", sales: 359817, "sell-through": 90.26 },
+    ],
+  },
+];
