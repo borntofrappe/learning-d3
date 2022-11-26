6d834a57583e539312aa88a20119d58b61afad19
Gabriele Corti <borntofrappe@gmail.com>
2022-11-26 15:33:09 +0100

    refactor svg with d3

--- Tally Chart/script.js
+++ Tally Chart/script.js
@@ -12,3 +12,31 @@ const data = [
   { month: "November", value: 19 },
   { month: "December", value: 19 },
 ];
+
+const defs = d3
+  .select("body")
+  .append("svg")
+  .style("display", "none")
+  .append("defs");
+
+defs.append("path").attr("id", "mark-0").attr("d", "M 0 0 v 3");
+defs.append("path").attr("id", "mark-1").attr("d", "M 1 0 v 3");
+defs.append("path").attr("id", "mark-2").attr("d", "M 2 0 v 3");
+defs.append("path").attr("id", "mark-3").attr("d", "M 3 0 v 3");
+defs.append("path").attr("id", "mark-4").attr("d", "M -0.5 2.4 l 4 -1.8");
+
+const svg = d3.select("body").append("svg").attr("viewBox", "0 0 5 5");
+const group = svg.append("g").attr("transform", "translate(1 1)");
+const groupMarks = group
+  .append("g")
+  .attr("fill", "none")
+  .attr("stroke", "currentColor")
+  .attr("stroke-width", "0.5")
+  .attr("stroke-linecap", "square");
+
+groupMarks
+  .selectAll("use")
+  .data(Array(5))
+  .enter()
+  .append("use")
+  .attr("href", (_, i) => `#mark-${i}`);
