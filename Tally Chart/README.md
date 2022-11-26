c33d5850ae970e4b9ee1812f7fabe4bb6b3f82ca
Gabriele Corti <borntofrappe@gmail.com>
2022-11-26 15:18:56 +0100

    first commit #32

---
+++ Tally Chart/README.md
@@ -0,0 +1,3 @@
+# Tally Chart
+
+Create a chart counting through a visual similar to [Tally marks](https://en.wikipedia.org/wiki/Tally_marks).
---
+++ Tally Chart/script.js
@@ -0,0 +1,14 @@
+const data = [
+  { month: "January", value: 19 },
+  { month: "February", value: 16 },
+  { month: "March", value: 15 },
+  { month: "April", value: 11 },
+  { month: "May", value: 9 },
+  { month: "June", value: 9 },
+  { month: "July", value: 10 },
+  { month: "August", value: 12 },
+  { month: "September", value: 14 },
+  { month: "October", value: 18 },
+  { month: "November", value: 19 },
+  { month: "December", value: 19 },
+];
