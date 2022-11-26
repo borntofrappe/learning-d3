f24c88e2bec2d2d2cde32ad90e7f5529ca52fcb1
Gabriele Corti <borntofrappe@gmail.com>
2022-11-26 22:15:14 +0100

    add frame
    frame lines and clip the path elements to the width and height

--- Random Octaves/script.js
+++ Random Octaves/script.js
@@ -3,40 +3,77 @@ const numberOctaves = 4;
 const width = 500;
 const height = 100;
 
+const margin = {
+  top: 5,
+  right: 10,
+  bottom: 5,
+  left: 10,
+};
+
 const lines = Array(numberOctaves)
   .fill()
   .map((_, i) => {
     const numberPoints = numberInitialPoints * (i + 1);
-    const heightLine = height / (i + 1);
+    const heightLine = 1 / (i + 1);
 
     return Array(numberPoints)
       .fill()
       .map((_, i, { length }) => {
-        const x = (width / (length - 1)) * i;
+        const x = (1 / (length - 1)) * i;
         const y = Math.random() * heightLine;
 
         return { x, y };
       });
   });
 
+const scaleX = d3.scaleLinear().range([0, width]);
+const scaleY = d3.scaleLinear().range([height, 0]);
+
 const line = d3
   .line()
-  .x(({ x }) => x)
-  .y(({ y }) => height - y)
+  .x(({ x }) => scaleX(x))
+  .y(({ y }) => scaleY(y))
   .curve(d3.curveCatmullRom);
 
 const svg = d3
   .select("body")
   .append("svg")
-  .attr("viewBox", `0 0 ${width} ${height}`);
+  .attr(
+    "viewBox",
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  );
+
+const defs = svg.append("defs");
+defs
+  .append("clipPath")
+  .attr("id", "clip-lines")
+  .append("rect")
+  .attr("width", width)
+  .attr("height", height);
 
 const group = svg
   .append("g")
+  .attr("transform", `translate(${margin.left} ${margin.right})`);
+
+const groupAxis = group.append("g");
+const groupData = group.append("g");
+
+groupAxis
+  .append("path")
+  .attr("fill", "none")
+  .attr("stroke", "currentColor")
+  .attr("stroke-width", "1")
+  .attr("d", `M 0 0 h ${width} v ${height} h ${-width}z`);
+
+groupData
+  .attr("clip-path", "url(#clip-lines)")
   .attr("fill", "none")
   .attr("stroke", "currentColor")
   .attr("stroke-width", "1");
 
-const groupLines = group.selectAll("g").data(lines).enter().append("g");
+const groupLines = groupData.selectAll("g").data(lines).enter().append("g");
 
 groupLines
   .append("path")
