ccf5a3ce6dfae52c9358ea5ef99f4abb4b5d8626
Gabriele Corti <borntofrappe@gmail.com>
2022-11-27 18:10:12 +0100

    introduce step sequence
    include controls to step through the visualization

--- Perlin-like Noise/script.js
+++ Perlin-like Noise/script.js
@@ -1,5 +1,3 @@
-const numberInitialPoints = 5;
-const numberOctaves = 4;
 const width = 500;
 const height = 100;
 
@@ -14,6 +12,144 @@ const padding = {
   x: 0,
   y: 20,
 };
+
+const root = d3.select("body").append("div").attr("id", "root");
+
+const header = root.append("header");
+header.append("h1").text("Perlin-like noise");
+
+header
+  .append("p")
+  .html(
+    "One way to create Perlin like noise is through a series of <em>octaves</em>, functions interpolating the position of random values."
+  );
+
+const svg = root
+  .append("svg")
+  .attr(
+    "viewBox",
+    `0 0 ${width + margin.left + margin.right + padding.x * 2} ${
+      height + margin.top + margin.bottom + padding.y * 2
+    }`
+  );
+
+const groupMargin = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left} ${margin.right})`);
+
+const groupFrame = groupMargin.append("g");
+
+groupFrame
+  .append("path")
+  .attr("fill", "none")
+  .attr("stroke", "currentColor")
+  .attr("stroke-width", "1")
+  .attr(
+    "d",
+    `M 0 0 h ${width + padding.x * 2} v ${height + padding.y * 2} h ${-(
+      width +
+      padding.x * 2
+    )}z`
+  );
+
+const controls = root.append("div");
+controls.append("h2").text("Step 1");
+controls
+  .append("button")
+  .text("Compute random values")
+  .on(
+    "click",
+    () => {
+      controls.transition().style("opacity", "0").style("visibility", "hidden");
+
+      const numberInitialPoints = 5;
+      const numberOctaves = 4;
+
+      const scaleX = d3.scaleLinear().range([0, width]);
+      const scaleY = d3.scaleLinear().range([height, 0]);
+
+      const line = d3
+        .line()
+        .x(({ x }) => scaleX(x))
+        .y(({ y }) => scaleY(y))
+        .curve(d3.curveCatmullRom);
+
+      const points = Array(numberInitialPoints)
+        .fill()
+        .map((_, i, { length }) => {
+          const x = (1 / (length - 1)) * i;
+          const y = Math.random();
+
+          return { x, y };
+        });
+
+      const transition = d3.transition().duration(500);
+
+      const groupPadding = groupMargin
+        .append("g")
+        .attr("transform", `translate(${padding.x} ${padding.y})`);
+
+      const groupPoints = groupPadding.append("g").attr("fill", "currentColor");
+
+      groupPoints
+        .selectAll("circle")
+        .data(points)
+        .enter()
+        .append("circle")
+        .attr("transform", ({ x, y }) => `translate(${scaleX(x)} ${scaleY(y)})`)
+        .attr("r", "0")
+        .transition(transition)
+        .delay((_, i) => 50 * i)
+        .attr("r", "5");
+
+      transition.on("end", () => {
+        controls.select("h2").text("Step 2");
+        controls.select("button").text("Interpolate between the points");
+
+        controls
+          .transition()
+          .delay(250)
+          .style("opacity", "1")
+          .style("visibility", "visible");
+
+        controls.select("button").on(
+          "click",
+          () => {
+            controls
+              .transition()
+              .style("opacity", "0")
+              .style("visibility", "hidden");
+
+            const transition = d3.transition().duration(500);
+            const groupOctaves = groupPadding
+              .append("g")
+              .attr("fill", "none")
+              .attr("stroke", "currentColor")
+              .attr("stroke-width", "1");
+
+            groupOctaves
+              .append("path")
+              .datum(points)
+              .attr("d", line)
+              .attr("pathLength", "1")
+              .attr("stroke-dasharray", "1")
+              .attr("stroke-dashoffset", "1")
+              .transition(transition)
+              .attr("stroke-dashoffset", "0");
+          },
+          {
+            once: true,
+          }
+        );
+      });
+    },
+    {
+      once: true,
+    }
+  );
+
+/*
+  
 
 const noise = Array(numberOctaves)
   .fill()
@@ -39,16 +175,6 @@ const line = d3
   .x(({ x }) => scaleX(x))
   .y(({ y }) => scaleY(y))
   .curve(d3.curveCatmullRom);
-
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr(
-    "viewBox",
-    `0 0 ${width + margin.left + margin.right + padding.x * 2} ${
-      height + margin.top + margin.bottom + padding.y * 2
-    }`
-  );
 
 const group = svg
   .append("g")
@@ -156,3 +282,4 @@ d3.select("body")
       once: true,
     }
   );
+*/
