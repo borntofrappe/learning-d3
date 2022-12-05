1fb7b21dd6d3b9dcb86982d11e72176e8c3d91fb
Gabriele Corti <borntofrappe@gmail.com>
2022-12-05 15:33:41 +0100

    first commit #38

---
+++ Legend Component/README.md
@@ -0,0 +1,3 @@
+# Legend Component
+
+Create a component function to annotate a visualization with a legend. Dispatch an event from on `mouseenter`.
---
+++ Legend Component/script.js
@@ -0,0 +1,6 @@
+const data = [
+  { value: 445757, label: "Solid fossil fuels" },
+  { value: 4554664, label: "Natural gas" },
+  { value: 6372414, label: "Petroleum" },
+  { value: 1766433, label: "Renewables" },
+];
