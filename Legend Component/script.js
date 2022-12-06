82b4a8b6369a2a5c23509d610d28a48dfeb01c90
Gabriele Corti <borntofrappe@gmail.com>
2022-12-06 10:10:37 +0100

    complete #38
    include component at the top of the script, label events

--- Legend Component/script.js
+++ Legend Component/script.js
@@ -1,3 +1,121 @@
+const legend = () => {
+  let scale = d3
+    .scaleOrdinal()
+    .domain(["tomato", "seagreen", "aqua"])
+    .range(["tomato", "seagreen", "aqua"]);
+
+  let width = 150;
+  let height = 200;
+  let fontSize = 24;
+  let roundingRect = 0.2;
+  let scaleOffset = d3.scaleBand();
+
+  const dispatch = d3.dispatch("enter-item", "leave-area");
+
+  const legend = (selection) => {
+    const labels = scale.domain();
+    scaleOffset.domain(labels).range([0, height]);
+
+    const rect = selection
+      .append("rect")
+      .attr("x", "-5")
+      .attr("width", width + "5")
+      .attr("height", height)
+      .attr("fill", "transparent")
+      .attr("opacity", "0");
+
+    const groups = selection
+      .selectAll("g")
+      .data(labels)
+      .enter()
+      .append("g")
+      .attr(
+        "transform",
+        (d) => `translate(0 ${scaleOffset(d) + scaleOffset.bandwidth() / 2})`
+      );
+
+    groups
+      .append("rect")
+      .attr("y", -fontSize / 2)
+      .attr("width", fontSize)
+      .attr("height", fontSize)
+      .attr("rx", fontSize * roundingRect)
+      .attr("fill", (d) => scale(d));
+
+    groups
+      .append("text")
+      .attr("x", fontSize + 10)
+      .attr("dominant-baseline", "central")
+      .attr("fill", "currentColor")
+      .attr("font-size", fontSize)
+      .text((d) => d);
+
+    groups.on("mouseenter", function (e, d) {
+      groups.attr("opacity", "0.1").attr("font-weight", "inherit");
+      d3.select(this).attr("opacity", "1").attr("font-weight", "700");
+
+      dispatch.call("enter-item", this, e, d);
+    });
+
+    rect.on("mouseleave", function (e) {
+      groups.attr("opacity", "1").attr("font-weight", "inherit");
+
+      dispatch.call("leave-area", this, e);
+    });
+  };
+
+  legend.scale = function (value) {
+    if (!arguments.length) return scale;
+
+    scale = value;
+    return this;
+  };
+
+  legend.width = function (value) {
+    if (!arguments.length) return width;
+
+    width = value;
+    return this;
+  };
+
+  legend.height = function (value) {
+    if (!arguments.length) return height;
+
+    height = value;
+    return this;
+  };
+
+  legend.scaleOffset = function (scale) {
+    if (!arguments.length) return scaleOffset;
+
+    scaleOffset = scale;
+    return this;
+  };
+
+  legend.fontSize = function (value) {
+    if (!arguments.length) return fontSize;
+
+    fontSize = value;
+    return this;
+  };
+
+  legend.roundingRect = function (value) {
+    if (!arguments.length) return roundingRect;
+
+    roundingRect = value;
+    return this;
+  };
+
+  legend.on = function () {
+    if (!arguments.length) return on;
+
+    dispatch.on.apply(dispatch, arguments);
+    return this;
+  };
+
+  return legend;
+};
+
 const data = [
   { value: 866.25, label: "Housing" },
   { value: 247.5, label: "Transportation" },
@@ -90,142 +208,25 @@ groupPaths
     (d, i) => `var(--color-${i + 1}, ${scaleColor(d.data.label)})`
   );
 
-const legend = () => {
-  let scale = d3
-    .scaleOrdinal()
-    .domain(["tomato", "seagreen", "aqua"])
-    .range(["tomato", "seagreen", "aqua"]);
+const legendComponent = legend()
+  .scale(scaleColor)
+  .width(legendWidth)
+  .height(size)
+  .fontSize(26)
+  .roundingRect(0.5)
+  .on("enter-item", (e, d) => {
+    groupPaths
+      .selectAll("path")
+      .attr("opacity", "0.1")
+      .filter(({ data }) => data.label === d)
+      .attr("opacity", "1");
 
-  let width = 150;
-  let height = 200;
-  let fontSize = 24;
-  let roundingRect = 0.2;
-  let scaleOffset = d3.scaleBand();
+    const { value } = pieData.find(({ data }) => data.label === d).data;
+    textHighlight.text(format(value));
+  })
+  .on("leave-area", function (e) {
+    groupPaths.selectAll("path").attr("opacity", "1");
+    textHighlight.text(format(total));
+  });
 
-  const dispatch = d3.dispatch("enter-label", "leave-legend");
-
-  const legend = (selection) => {
-    const labels = scale.domain();
-    scaleOffset.domain(labels).range([0, height]);
-
-    const rect = selection
-      .append("rect")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("fill", "transparent")
-      .attr("opacity", "0");
-
-    const groups = selection
-      .selectAll("g")
-      .data(labels)
-      .enter()
-      .append("g")
-      .attr(
-        "transform",
-        (d) => `translate(0 ${scaleOffset(d) + scaleOffset.bandwidth() / 2})`
-      );
-
-    groups
-      .append("rect")
-      .attr("y", -fontSize / 2)
-      .attr("width", fontSize)
-      .attr("height", fontSize)
-      .attr("rx", fontSize * roundingRect)
-      .attr("fill", (d) => scale(d));
-
-    groups
-      .append("text")
-      .attr("x", fontSize + 10)
-      .attr("dominant-baseline", "central")
-      .attr("fill", "currentColor")
-      .attr("font-size", fontSize)
-      .text((d) => d);
-
-    groups.on("mouseenter", function (e, d) {
-      groups.attr("opacity", "0.1").attr("font-weight", "inherit");
-      d3.select(this).attr("opacity", "1").attr("font-weight", "700");
-
-      dispatch.call("enter-label", this, e, d);
-    });
-
-    rect.on("mouseleave", function (e) {
-      groups.attr("opacity", "1").attr("font-weight", "inherit");
-
-      dispatch.call("leave-legend", this, e);
-    });
-  };
-
-  legend.scale = function (value) {
-    if (!arguments.length) return scale;
-
-    scale = value;
-    return this;
-  };
-
-  legend.width = function (value) {
-    if (!arguments.length) return width;
-
-    width = value;
-    return this;
-  };
-
-  legend.height = function (value) {
-    if (!arguments.length) return height;
-
-    height = value;
-    return this;
-  };
-
-  legend.scaleOffset = function (scale) {
-    if (!arguments.length) return scaleOffset;
-
-    scaleOffset = scale;
-    return this;
-  };
-
-  legend.fontSize = function (value) {
-    if (!arguments.length) return fontSize;
-
-    fontSize = value;
-    return this;
-  };
-
-  legend.roundingRect = function (value) {
-    if (!arguments.length) return roundingRect;
-
-    roundingRect = value;
-    return this;
-  };
-
-  legend.on = function () {
-    if (!arguments.length) return on;
-
-    dispatch.on.apply(dispatch, arguments);
-    return this;
-  };
-
-  return legend;
-};
-
-groupLegend.call(
-  legend()
-    .scale(scaleColor)
-    .width(legendWidth)
-    .height(size)
-    .fontSize(24)
-    .roundingRect(0.5)
-    .on("enter-label", (e, d) => {
-      groupPaths
-        .selectAll("path")
-        .attr("opacity", "0.1")
-        .filter(({ data }) => data.label === d)
-        .attr("opacity", "1");
-
-      const { value } = pieData.find(({ data }) => data.label === d).data;
-      textHighlight.text(format(value));
-    })
-    .on("leave-legend", function (e) {
-      groupPaths.selectAll("path").attr("opacity", "1");
-      textHighlight.text(format(total));
-    })
-);
+groupLegend.call(legendComponent);
