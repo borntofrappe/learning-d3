fbbcd8625886f488fc986bb3c423ee9370298f4f
Gabriele Corti <borntofrappe@gmail.com>
2022-12-17 11:26:38 +0100

    complete stacked bar chart component

--- Grouped Bar Chart/script.js
+++ Grouped Bar Chart/script.js
@@ -1,3 +1,143 @@
+const stackedBarChartComponent = () => {
+  let data;
+  let keys;
+
+  const stack = d3.stack();
+  const scaleX = d3.scaleBand().range([0, width]).padding(0.4);
+  const scaleY = d3.scaleLinear().range([height, 0]);
+
+  const axisX = d3
+    .axisBottom(scaleX)
+    .tickFormat((d) => timeFormat(d))
+    .tickSize(0)
+    .tickPadding(10);
+
+  const axisY = d3
+    .axisLeft(scaleY)
+    .ticks(4)
+    .tickFormat((d) => (d ? `${d} h` : ""))
+    .tickSize(0)
+    .tickPadding(6);
+
+  const timeParse = d3.timeParse("%Y-%m-%d");
+  const timeFormat = d3.timeFormat("%b %-d");
+
+  const stackedBar = (context) => {
+    const series = stack.keys(keys)(data);
+
+    const selection = context.selection ? context.selection() : context;
+
+    scaleX.domain(data.map((d) => timeParse(d.date)));
+    scaleY.domain([0, d3.max(series[series.length - 1], (d) => d[1])]);
+
+    let groupAxisX = selection.selectAll("g.axis-x").data([null]);
+    let groupAxisY = selection.selectAll("g.axis-y").data([null]);
+    let groupsSeries = selection
+      .selectAll("g.series")
+      .data(series, (d) => d.key);
+
+    groupAxisX = groupAxisX.merge(
+      groupAxisX
+        .enter()
+        .append("g")
+        .attr("class", "axis-x")
+        .attr("transform", `translate(0 ${height})`)
+    );
+
+    groupAxisY = groupAxisY.merge(
+      groupAxisY.enter().append("g").attr("class", "axis-y")
+    );
+
+    const groupsSeriesEnter = groupsSeries
+      .enter()
+      .append("g")
+      .attr("class", "series")
+      .attr("fill", (d) => scaleColor(d.key));
+
+    let groupsSeriesExit = groupsSeries.exit();
+
+    groupsSeriesEnter
+      .selectAll("rect")
+      .data((d) => d)
+      .enter()
+      .append("rect")
+      .attr("x", (d) => scaleX(timeParse(d.data.date)))
+      .attr("width", scaleX.bandwidth())
+      .attr("y", (d) => scaleY(d[1]))
+      .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
+
+    if (context !== selection) {
+      groupAxisX = groupAxisX.transition(context);
+      groupAxisY = groupAxisY.transition(context);
+
+      groupsSeriesEnter
+        .selectAll("rect")
+        .attr("y", height)
+        .attr("height", "0")
+        .transition(context)
+        .attr("y", (d) => scaleY(d[1]))
+        .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
+
+      groupsSeries
+        .selectAll("rect")
+        .data((d) => d)
+        .join(
+          (enter) => {
+            enter
+              .append("rect")
+              .attr("x", (d) => scaleX(timeParse(d.data.date)))
+              .attr("width", scaleX.bandwidth())
+              .attr("y", (d) => scaleY(d[0]))
+              .attr("height", "0")
+              .transition(context)
+              .attr("y", (d) => scaleY(d[1]))
+              .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
+          },
+          (update) => {
+            update
+              .transition(context)
+              .attr("x", (d) => scaleX(timeParse(d.data.date)))
+              .attr("width", scaleX.bandwidth())
+              .attr("y", (d) => scaleY(d[1]))
+              .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
+          },
+          (exit) => {
+            exit.transition(context).attr("height", "0").remove();
+          }
+        );
+
+      groupsSeriesExit
+        .selectAll("rect")
+        .transition(context)
+        .attr("y", height)
+        .attr("height", "0");
+
+      groupsSeriesExit = groupsSeriesExit.transition(context);
+    }
+
+    groupsSeriesExit.remove();
+
+    groupAxisX.call(axisX);
+    groupAxisY.call(axisY);
+  };
+
+  stackedBar.data = function (d) {
+    if (!arguments.length) return data;
+
+    data = d;
+    return this;
+  };
+
+  stackedBar.keys = function (d) {
+    if (!arguments.length) return keys;
+
+    keys = d;
+    return this;
+  };
+
+  return stackedBar;
+};
+
 // prettier-ignore
 const data = [
   { "date": "2022-12-03", "Email & messages": 1, "Writing": 2, "Coding": 3, "Designing": 1, "Research": 0, "Paperwork": 0, "Planning": 1 },
@@ -16,405 +156,78 @@ const data = [
   { "date": "2022-12-16", "Email & messages": 1, "Writing": 1, "Coding": 2, "Designing": 2, "Research": 0, "Paperwork": 0, "Planning": 1 }
 ];
 
-(() => {
-  const keys = Object.keys(data[0]).filter((d) => d !== "date");
-  const scaleColor = d3.scaleOrdinal(d3.schemeTableau10).domain(keys);
+const keys = Object.keys(data[0]).filter((d) => d !== "date");
+const scaleColor = d3.scaleOrdinal(d3.schemeTableau10).domain(keys);
 
-  const article = d3.select("body").append("article");
-  article.append("h2").text("Stacked bar chart — Collapse");
-  const legendKeys = article.append("div").attr("class", "legend");
+const article = d3.select("body").append("article");
+article.append("h2").text("Stacked Bar Chart");
+const legendKeys = article.append("div").attr("class", "legend");
 
-  const legendsKeys = legendKeys
-    .selectAll("span")
-    .data(keys)
-    .enter()
-    .append("span")
-    .text((d) => d);
+const legendsKeys = legendKeys
+  .selectAll("span")
+  .data(keys)
+  .enter()
+  .append("span")
+  .text((d) => d);
 
-  legendsKeys
-    .append("svg")
-    .attr("viewBox", "0 0 1 1")
-    .style("height", "1em")
-    .attr("fill", (d) => scaleColor(d))
-    .append("rect")
-    .attr("width", "1")
-    .attr("height", "1");
+legendsKeys
+  .append("svg")
+  .attr("viewBox", "0 0 1 1")
+  .style("height", "1em")
+  .attr("fill", (d) => scaleColor(d))
+  .append("rect")
+  .attr("width", "1")
+  .attr("height", "1");
 
-  const width = 600;
-  const height = 375;
-  const margin = {
-    top: 30,
-    bottom: 25,
-    left: 35,
-    right: 10,
-  };
+const width = 600;
+const height = 375;
+const margin = {
+  top: 30,
+  bottom: 25,
+  left: 35,
+  right: 10,
+};
 
-  const svg = article
-    .append("svg")
-    .attr(
-      "viewBox",
-      `0 0 ${width + margin.left + margin.right} ${
-        height + margin.top + margin.bottom
-      }`
-    );
+const svg = article
+  .append("svg")
+  .attr("class", "stacked-bar-chart")
+  .attr(
+    "viewBox",
+    `0 0 ${width + margin.left + margin.right} ${
+      height + margin.top + margin.bottom
+    }`
+  );
 
-  const group = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left} ${margin.top})`);
+const group = svg
+  .append("g")
+  .attr("transform", `translate(${margin.left} ${margin.top})`);
 
-  const stackedBar = () => {
-    let data;
-    let keys;
+const stackedBarChart = stackedBarChartComponent().data(data).keys(keys);
+group.call(stackedBarChart);
 
-    const stack = d3.stack();
-    const scaleX = d3.scaleBand().range([0, width]).padding(0.4);
-    const scaleY = d3.scaleLinear().range([height, 0]);
+legendsKeys.style("cursor", "pointer").on("click", function (e, key) {
+  e.stopPropagation();
 
-    const axisX = d3
-      .axisBottom(scaleX)
-      .tickFormat((d) => timeFormat(d))
-      .tickSize(0)
-      .tickPadding(10);
-
-    const axisY = d3
-      .axisLeft(scaleY)
-      .ticks(4)
-      .tickFormat((d) => (d ? `${d} h` : ""))
-      .tickSize(0)
-      .tickPadding(6);
-
-    const valueFormat = (d) => `${d3.format(".2")(d)}h`;
-    const timeParse = d3.timeParse("%Y-%m-%d");
-    const timeFormat = d3.timeFormat("%b %-d");
-
-    const stackedBar = (context) => {
-      const series = stack.keys(keys)(data);
-
-      const selection = context.selection ? context.selection() : context;
-
-      scaleX.domain(data.map((d) => timeParse(d.date)));
-      scaleY.domain([0, d3.max(series[series.length - 1], (d) => d[1])]);
-
-      let groupAxisX = selection.selectAll("g.axis-x").data([null]);
-      let groupAxisY = selection.selectAll("g.axis-y").data([null]);
-      let groupsSeries = selection
-        .selectAll("g.series")
-        .data(series, (d) => d.key);
-
-      groupAxisX = groupAxisX.merge(
-        groupAxisX
-          .enter()
-          .append("g")
-          .attr("class", "axis-x")
-          .attr("transform", `translate(0 ${height})`)
-      );
-
-      groupAxisY = groupAxisY.merge(
-        groupAxisY.enter().append("g").attr("class", "axis-y")
-      );
-
-      const groupsSeriesEnter = groupsSeries
-        .enter()
-        .append("g")
-        .attr("class", "series")
-        .attr("fill", (d) => scaleColor(d.key));
-
-      let groupsSeriesExit = groupsSeries.exit();
-
-      groupsSeriesEnter
-        .selectAll("rect")
-        .data((d) => d)
-        .enter()
-        .append("rect")
-        .attr("x", (d) => scaleX(timeParse(d.data.date)))
-        .attr("width", scaleX.bandwidth())
-        .attr("y", (d) => scaleY(d[1]))
-        .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
-
-      if (context !== selection) {
-        groupAxisX = groupAxisX.transition(context);
-        groupAxisY = groupAxisY.transition(context);
-
-        groupsSeriesEnter
-          .selectAll("rect")
-          .attr("y", height)
-          .attr("height", "0")
-          .transition(context)
-          .attr("y", (d) => scaleY(d[1]))
-          .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
-
-        groupsSeries
-          .selectAll("rect")
-          .data((d) => d)
-          .join(
-            (enter) => {
-              enter
-                .append("rect")
-                .attr("x", (d) => scaleX(timeParse(d.data.date)))
-                .attr("width", scaleX.bandwidth())
-                .attr("y", (d) => scaleY(d[0]))
-                .attr("height", "0")
-                .transition(context)
-                .attr("y", (d) => scaleY(d[1]))
-                .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
-            },
-            (update) => {
-              update
-                .transition(context)
-                .attr("x", (d) => scaleX(timeParse(d.data.date)))
-                .attr("width", scaleX.bandwidth())
-                .attr("y", (d) => scaleY(d[1]))
-                .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
-            },
-            (exit) => {
-              exit.transition(context).attr("height", "0").remove();
-            }
-          );
-
-        groupsSeriesExit
-          .selectAll("rect")
-          .transition(context)
-          .attr("y", height)
-          .attr("height", "0");
-
-        groupsSeriesExit = groupsSeriesExit.transition(context);
-      }
-
-      groupsSeriesExit.remove();
-
-      groupAxisX.call(axisX);
-      groupAxisY.call(axisY);
-    };
-
-    stackedBar.data = function (d) {
-      if (!arguments.length) return data;
-
-      data = d;
-      return this;
-    };
-
-    stackedBar.keys = function (d) {
-      if (!arguments.length) return keys;
-
-      keys = d;
-      return this;
-    };
-
-    return stackedBar;
-  };
-
-  const myStackedBar = stackedBar().data(data).keys(keys);
-  group.call(myStackedBar);
-
-  legendsKeys.style("cursor", "pointer").on("click", function (e, key) {
-    e.stopPropagation();
-
-    legendsKeys
-      .filter((d) => d !== key)
-      .transition()
-      .style("opacity", "0.25")
-      .style("filter", "grayscale(1)");
-
-    group
-      .transition()
-      .duration(500)
-      .ease(d3.easeQuadInOut)
-      .call(myStackedBar.keys([key]));
-
-    d3.select(this).style("opacity", "1").style("filter", "grayscale(0)");
-  });
-
-  article.on("click", () => {
-    legendsKeys
-      .transition()
-      .style("opacity", "1")
-      .style("filter", "grayscale(0)");
-
-    group
-      .transition()
-      .duration(500)
-      .ease(d3.easeQuadInOut)
-      .call(myStackedBar.keys(keys));
-  });
-})();
-
-(() => {
-  const width = 500;
-  const height = 350;
-  const margin = {
-    top: 30,
-    bottom: 25,
-    left: 35,
-    right: 10,
-  };
-
-  const valueFormat = (d) => `${d3.format(".2")(d)}h`;
-  const timeParse = d3.timeParse("%Y-%m-%d");
-  const timeFormat = d3.timeFormat("%b %-d");
-
-  const keys = Object.keys(data[0]).filter((d) => d !== "date");
-
-  const stack = d3.stack().keys(keys);
-
-  const series = stack(data);
-
-  const scaleX = d3
-    .scaleBand()
-    .domain(data.map((d) => timeParse(d.date)))
-    .range([0, width])
-    .padding(0.4);
-
-  const scaleY = d3
-    .scaleLinear()
-    .domain([0, d3.max(series[series.length - 1], (d) => d[1])])
-    .range([height, 0]);
-
-  const scaleColor = d3.scaleOrdinal(d3.schemeTableau10).domain(keys);
-
-  const axisX = d3
-    .axisBottom(scaleX)
-    .tickFormat((d) => timeFormat(d))
-    .tickSize(0)
-    .tickPadding(10);
-
-  const axisY = d3
-    .axisLeft(scaleY)
-    .ticks(4)
-    .tickFormat((d) => (d ? `${d} h` : ""))
-    .tickSize(0)
-    .tickPadding(6);
-
-  const article = d3.select("body").append("article");
-  article.append("h2").text("Stacked bar chart — Highlight");
-  const legendKeys = article.append("div").attr("class", "legend");
-
-  const legendsKeys = legendKeys
-    .selectAll("span")
-    .data(keys)
-    .enter()
-    .append("span")
-    .text((d) => d);
+  const transition = d3.transition().duration(500).ease(d3.easeQuadInOut);
 
   legendsKeys
-    .append("svg")
-    .attr("viewBox", "0 0 1 1")
-    .style("height", "1em")
-    .attr("fill", (d) => scaleColor(d))
-    .append("rect")
-    .attr("width", "1")
-    .attr("height", "1");
+    .filter((d) => d !== key)
+    .transition(transition)
+    .style("opacity", "0.25")
+    .style("filter", "grayscale(1)");
 
-  const svg = article
-    .append("svg")
-    .attr(
-      "viewBox",
-      `0 0 ${width + margin.left + margin.right} ${
-        height + margin.top + margin.bottom
-      }`
-    );
+  group.transition(transition).call(stackedBarChart.keys([key]));
 
-  const group = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left} ${margin.top})`);
+  d3.select(this).style("opacity", "1").style("filter", "grayscale(0)");
+});
 
-  const groupData = group.append("g");
-  const groupAxis = group.append("g");
-  const groupDetails = group.append("g").style("pointer-events", "none");
+article.on("click", () => {
+  const transition = d3.transition().duration(500).ease(d3.easeQuadInOut);
 
-  const groupAxisX = groupAxis
-    .append("g")
-    .attr("transform", `translate(0 ${height})`)
-    .call(axisX);
-  const groupAxisY = groupAxis.append("g").call(axisY);
+  legendsKeys
+    .transition(transition)
+    .style("opacity", "1")
+    .style("filter", "grayscale(0)");
 
-  groupAxisY.select("path").remove();
-
-  groupAxisX.selectAll("text").attr("font-size", "8");
-  groupAxisY.selectAll("text").attr("font-size", "10");
-
-  const groupsData = groupData
-    .selectAll("g")
-    .data(series)
-    .enter()
-    .append("g")
-    .attr("fill", (d) => scaleColor(d.key));
-
-  groupsData
-    .selectAll("rect")
-    .data((d) => d)
-    .enter()
-    .append("rect")
-    .attr("x", (d) => scaleX(timeParse(d.data.date)))
-    .attr("width", scaleX.bandwidth())
-    .attr("y", (d) => scaleY(d[1]))
-    .attr("height", (d) => scaleY(d[0]) - scaleY(d[1]));
-
-  legendsKeys.style("cursor", "pointer").on("click", function (e, d) {
-    e.stopPropagation();
-
-    legendsKeys
-      .filter((key) => key !== d)
-      .transition()
-      .style("opacity", "0.25")
-      .style("filter", "grayscale(1)");
-
-    groupsData
-      .filter(({ key }) => key !== d)
-      .transition()
-      .attr("opacity", "0.25")
-      .style("filter", "grayscale(1)");
-
-    d3.select(this).style("opacity", "1").style("filter", "grayscale(0)");
-
-    groupsData
-      .filter(({ key }) => key === d)
-      .attr("opacity", "1")
-      .style("filter", "grayscale(0)");
-
-    const dataDetails = series.find(({ key }) => key === d);
-    groupDetails
-      .selectAll("text")
-      .data(dataDetails)
-      .join(
-        (enter) => {
-          enter
-            .append("text")
-            .attr("font-size", "10")
-            .attr("font-weight", "700")
-            .attr("text-anchor", "middle")
-            .attr("x", scaleX.bandwidth() / 2)
-            .attr("y", -5)
-            .attr(
-              "transform",
-              (d) => `translate(${scaleX(timeParse(d.data.date))} 0)`
-            )
-            .text((d) => valueFormat(d[1] - d[0]));
-        },
-        (update) => {
-          update
-            .attr(
-              "transform",
-              (d) => `translate(${scaleX(timeParse(d.data.date))} 0)`
-            )
-            .text((d) => valueFormat(d[1] - d[0]));
-        },
-        (exit) => {
-          exit.remove();
-        }
-      );
-  });
-
-  article.on("click", () => {
-    legendsKeys
-      .transition()
-      .style("opacity", "1")
-      .style("filter", "grayscale(0)");
-
-    groupsData
-      .transition()
-      .attr("opacity", "1")
-      .style("filter", "grayscale(0)");
-
-    groupDetails.selectAll("*").remove();
-  });
-})();
+  group.transition(transition).call(stackedBarChart.keys(keys));
+});
