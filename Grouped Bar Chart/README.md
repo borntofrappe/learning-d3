d3424ba806bd3af2e583620710f55a49f944076b
Gabriele Corti <borntofrappe@gmail.com>
2022-12-18 22:34:43 +0100

    update dataset

--- Grouped Bar Chart/README.md
+++ Grouped Bar Chart/README.md
@@ -2,6 +2,10 @@
 
 Map data for multiple categories and days in a grouped bar chart.
 
+## Data
+
+[lemonde.fr](https://www.lemonde.fr/les-decodeurs/article/2022/11/30/coupe-du-monde-2022-quel-est-le-destin-des-premiers-de-chaque-poule-pour-la-suite-de-la-competition_6152373_4355770.html)
+
 ## Notes
 
 An alternative for a [stacked bar chart](), a grouped bar chart maps categories in bars side by side instead of one above the other. You are able to better estimate the value of the individual categories, but cannot estimate the evolution, the overall trend over time. What is more, the visualization becomes cumbersome for large datasets.
--- Grouped Bar Chart/script.js
+++ Grouped Bar Chart/script.js
@@ -1,79 +1,60 @@
 // prettier-ignore
 const data = [
-    { "date": "2022-12-03", "Email & messages": 1, "Writing": 2, "Coding": 3, "Designing": 1, "Research": 0, "Paperwork": 0, "Planning": 1 },
-    { "date": "2022-12-04", "Email & messages": 0, "Writing": 1, "Coding": 2, "Designing": 1, "Research": 1, "Paperwork": 0, "Planning": 0 },
-    { "date": "2022-12-05", "Email & messages": 0, "Writing": 1, "Coding": 2, "Designing": 0, "Research": 1, "Paperwork": 2, "Planning": 0 },
-    { "date": "2022-12-06", "Email & messages": 0, "Writing": 1, "Coding": 2, "Designing": 2, "Research": 0, "Paperwork": 0, "Planning": 0 },
-    { "date": "2022-12-07", "Email & messages": 0, "Writing": 0, "Coding": 5, "Designing": 0, "Research": 0, "Paperwork": 0, "Planning": 2 },
-    { "date": "2022-12-08", "Email & messages": 0, "Writing": 1, "Coding": 3, "Designing": 1, "Research": 0, "Paperwork": 0, "Planning": 0 },
-    { "date": "2022-12-09", "Email & messages": 0, "Writing": 1, "Coding": 2, "Designing": 2, "Research": 0, "Paperwork": 0, "Planning": 1 },
-    { "date": "2022-12-10", "Email & messages": 0, "Writing": 3, "Coding": 2, "Designing": 0, "Research": 0, "Paperwork": 0, "Planning": 1 },
-    { "date": "2022-12-11", "Email & messages": 1, "Writing": 0, "Coding": 4, "Designing": 0, "Research": 2, "Paperwork": 0, "Planning": 1 },
-    { "date": "2022-12-12", "Email & messages": 0, "Writing": 2, "Coding": 0, "Designing": 3, "Research": 1, "Paperwork": 0, "Planning": 0 },
-    { "date": "2022-12-13", "Email & messages": 0, "Writing": 0, "Coding": 1, "Designing": 2, "Research": 1, "Paperwork": 2, "Planning": 0 },
-    { "date": "2022-12-14", "Email & messages": 1, "Writing": 3, "Coding": 1, "Designing": 1, "Research": 0, "Paperwork": 1, "Planning": 0 },
-    { "date": "2022-12-15", "Email & messages": 1, "Writing": 0, "Coding": 4, "Designing": 1, "Research": 0, "Paperwork": 0, "Planning": 0 },
-    { "date": "2022-12-16", "Email & messages": 1, "Writing": 1, "Coding": 2, "Designing": 2, "Research": 0, "Paperwork": 0, "Planning": 1 }
+    { "edition": "1994", "Final winner": 1, "Finalists": 2, "Semi-finalists": 4, "Quarter-finalists": 8 },
+    { "edition": "1998", "Final winner": 1, "Finalists": 2, "Semi-finalists": 3, "Quarter-finalists": 6 },
+    { "edition": "2002", "Final winner": 1, "Finalists": 2, "Semi-finalists": 3, "Quarter-finalists": 4 },
+    { "edition": "2006", "Final winner": 1, "Finalists": 1, "Semi-finalists": 3, "Quarter-finalists": 6 },
+    { "edition": "2010", "Final winner": 1, "Finalists": 2, "Semi-finalists": 4, "Quarter-finalists": 7 },
+    { "edition": "2014", "Final winner": 1, "Finalists": 2, "Semi-finalists": 4, "Quarter-finalists": 8 },
+    { "edition": "2018", "Final winner": 1, "Finalists": 2, "Semi-finalists": 3, "Quarter-finalists": 6 },
+    { "edition": "2022", "Final winner": 1, "Finalists": 2, "Semi-finalists": 3, "Quarter-finalists": 7 },
   ];
 
-const keys = Object.keys(data[0]).filter((d) => d !== "date");
+const keys = Object.keys(data[0]).filter((d) => d !== "edition");
 
-const width = 600;
-const height = 375;
+const width = 400;
+const height = 300;
+
 const margin = {
-  top: 10,
-  bottom: 25,
+  top: 25,
+  bottom: 10,
   left: 40,
   right: 10,
 };
 
-const timeParse = d3.timeParse("%Y-%m-%d");
-const timeFormat = d3.timeFormat("%b %-d");
-
-const yMax = d3.max(data, (d) =>
+const xMax = d3.max(data, (d) =>
   d3.max(
     Object.entries(d)
-      .filter((d) => d[0] !== "date")
+      .filter((d) => d[0] !== "edition")
       .map((d) => d[1])
   )
 );
 
-const xScale = d3
+const yScale = d3
   .scaleBand()
-  .domain(data.map((d) => d.date))
-  .range([0, width])
-  .padding(0.05);
+  .domain(data.map((d) => d.edition))
+  .range([0, height])
+  .padding(0.3);
 
-const xScale2 = d3
+const yScale2 = d3
   .scaleBand()
   .domain(keys)
-  .range([0, xScale.bandwidth()])
-  .padding(0.15);
+  .range([0, yScale.bandwidth()])
+  .padding(0.3);
 
-const yScale = d3.scaleLinear().domain([0, yMax]).range([height, 0]);
+const xScale = d3.scaleLinear().domain([0, xMax]).range([0, width]);
 
 const colorScale = d3
-  .scaleOrdinal(d3.schemeTableau10)
+  .scaleOrdinal(d3.schemePastel2)
   .domain(keys)
   .unknown("currentColor");
 
-const xAxis = d3
-  .axisBottom(xScale)
-  .tickSize(0)
-  .tickPadding(10)
-  .tickFormat((d) => timeFormat(timeParse(d)));
-
-const yAxis = d3
-  .axisLeft(yScale)
-  .ticks(4)
-  .tickSize(0)
-  .tickPadding(6)
-  .tickFormat((d) => (d ? `${d} h` : ""));
+const xAxis = d3.axisTop(xScale).tickSize(0).tickPadding(10);
+const yAxis = d3.axisLeft(yScale).tickSize(0).tickPadding(6);
 
 const svg = d3
   .select("body")
   .append("svg")
-  .attr("class", "chart")
   .attr(
     "viewBox",
     `0 0 ${width + margin.left + margin.right} ${
@@ -85,15 +66,10 @@ const group = svg
   .append("g")
   .attr("transform", `translate(${margin.left} ${margin.top})`);
 
-const groupAxis = group.append("g");
 const groupData = group.append("g");
+const groupAxis = group.append("g");
 
-groupAxis
-  .append("g")
-  .attr("class", "axis-x")
-  .attr("transform", `translate(0 ${height})`)
-  .call(xAxis);
-
+groupAxis.append("g").attr("class", "axis-x").call(xAxis);
 groupAxis.append("g").attr("class", "axis-y").call(yAxis);
 
 const groupsData = groupData
@@ -101,15 +77,14 @@ const groupsData = groupData
   .data(data)
   .enter()
   .append("g")
-  .attr("transform", (d) => `translate(${xScale(d.date)} 0)`);
+  .attr("transform", (d) => `translate(0 ${yScale(d.edition)})`);
 
 groupsData
   .selectAll("rect")
-  .data((d) => Object.entries(d).filter((d) => d[0] !== "date"))
+  .data((d) => Object.entries(d).filter((d) => d[0] !== "edition"))
   .enter()
   .append("rect")
-  .attr("x", (d) => xScale2(d[0]))
-  .attr("y", (d) => yScale(d[1]))
-  .attr("width", xScale2.bandwidth())
-  .attr("height", (d) => height - yScale(d[1]))
+  .attr("y", (d) => yScale2(d[0]))
+  .attr("width", (d) => xScale(d[1]))
+  .attr("height", yScale2.bandwidth())
   .attr("fill", (d) => colorScale(d[0]));
