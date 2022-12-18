c367df33fd664d67119db46a96d5c41b985e3a73
Gabriele Corti <borntofrappe@gmail.com>
2022-12-18 16:20:25 +0100

    draw data points

--- Grouped Bar Chart/script.js
+++ Grouped Bar Chart/script.js
@@ -1,5 +1,5 @@
 // prettier-ignore
-const dataset = [
+const data = [
     { "date": "2022-12-03", "Email & messages": 1, "Writing": 2, "Coding": 3, "Designing": 1, "Research": 0, "Paperwork": 0, "Planning": 1 },
     { "date": "2022-12-04", "Email & messages": 0, "Writing": 1, "Coding": 2, "Designing": 1, "Research": 1, "Paperwork": 0, "Planning": 0 },
     { "date": "2022-12-05", "Email & messages": 0, "Writing": 1, "Coding": 2, "Designing": 0, "Research": 1, "Paperwork": 2, "Planning": 0 },
@@ -16,8 +16,6 @@ const dataset = [
     { "date": "2022-12-16", "Email & messages": 1, "Writing": 1, "Coding": 2, "Designing": 2, "Research": 0, "Paperwork": 0, "Planning": 1 }
   ];
 
-const datapoints = 5;
-const data = dataset.slice(0, datapoints);
 const keys = Object.keys(data[0]).filter((d) => d !== "date");
 
 const width = 600;
@@ -43,9 +41,21 @@ const yMax = d3.max(data, (d) =>
 const xScale = d3
   .scaleBand()
   .domain(data.map((d) => d.date))
-  .range([0, width]);
+  .range([0, width])
+  .padding(0.05);
+
+const xScale2 = d3
+  .scaleBand()
+  .domain(keys)
+  .range([0, xScale.bandwidth()])
+  .padding(0.15);
 
 const yScale = d3.scaleLinear().domain([0, yMax]).range([height, 0]);
+
+const colorScale = d3
+  .scaleOrdinal(d3.schemeTableau10)
+  .domain(keys)
+  .unknown("currentColor");
 
 const xAxis = d3
   .axisBottom(xScale)
@@ -85,3 +95,21 @@ groupAxis
   .call(xAxis);
 
 groupAxis.append("g").attr("class", "axis-y").call(yAxis);
+
+const groupsData = groupData
+  .selectAll("g")
+  .data(data)
+  .enter()
+  .append("g")
+  .attr("transform", (d) => `translate(${xScale(d.date)} 0)`);
+
+groupsData
+  .selectAll("rect")
+  .data((d) => Object.entries(d).filter((d) => d[0] !== "date"))
+  .enter()
+  .append("rect")
+  .attr("x", (d) => xScale2(d[0]))
+  .attr("y", (d) => yScale(d[1]))
+  .attr("width", xScale2.bandwidth())
+  .attr("height", (d) => height - yScale(d[1]))
+  .attr("fill", (d) => colorScale(d[0]));
